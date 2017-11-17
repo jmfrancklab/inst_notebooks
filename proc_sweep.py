0ee4866b484cf7b3ff75c4f9cd01d9fbdb04dc2f
John Franck (where?) <jmfranck@syr.edu>
2017-11-17 15:33:58 -0500

    playing with ratios

--- proc_sweep.py
+++ proc_sweep.py
@@ -11,10 +11,7 @@ for j,thisfreq in enumerate(f_axis):
             directory=getDATADIR(exp_type='test_equip')
             ).set_units('t','s') # why are units not already set?
     d.ft('t',shift=True)
-    d = d['t':(0,40e6)] # analytic signal and low-pass
-    d.ift('t')
-    d *= d.fromaxis('t',
-            lambda x: exp(-1j*2*pi*mixdown*x))
+    d = d['t':(0,40e6)] # throw out negative frequencies and low-pass
     if j == 0:
         collated = ndshape(d)
         collated += ('f_pulse',len(f_axis))
@@ -22,11 +19,29 @@ for j,thisfreq in enumerate(f_axis):
         # {{{ note to self: this should NOT be required
         #     need to add/move labels to ndshape
         collated.setaxis('t', d.getaxis('t')).setaxis(
-                        'ch', d.getaxis('t')).setaxis(
+                'ch', d.getaxis('t')).setaxis(
                         'f_pulse', f_axis)
+        collated.set_units('t','Hz').set_units('f_pulse','Hz')
+        #collated.set_ft_prop('t') #shouldn't be required
         # }}}
+    # we should really do a lot of the above inside the acquisition routine
     collated['f_pulse',j] = d
 with figlist_var(filename='sweep_171116.pdf') as fl:
     collated.reorder('ch') # move ch first (outside)
+    collated.ift('t')
+    collated *= collated.fromaxis('t',
+            lambda x: exp(-1j*2*pi*mixdown*x))
     fl.next('analytic signal, raw')
     fl.image(collated)
+    ratio = collated['ch',1]/collated['ch',0]
+    fl.next('ratio ch2 to ch1')
+    fl.image(ratio)
+    fl.next('ratio, abs over safe range')
+    fl.image(abs(ratio['t':(11e-6,13e-6)]))
+    # {{{ because I have amplitudes that blow up, do the following:
+    #     (maybe I should be dividing in the frequency domain?
+    ratio /= abs(ratio)
+    ratio *= abs(collated['ch',1])
+    # }}}
+    fl.next('phase difference ch2 to ch1')
+    fl.image(ratio)
