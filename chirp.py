f3f7277dd82a496019c7f9bedaa03c9972cb5446
Alec Beaton (SU Network) <aabeaton@syr.edu>
2017-12-21 20:20:19 -0500

    updated chirp file date, created seaprate processing file for the control to compare

--- chirp.py
+++ chirp.py
@@ -41,10 +41,10 @@ data = concat(datalist,'ch').reorder('t')
 j = 1
 try_again = True
 while try_again:
-    data_name = 'capture%d_171220'%j
+    data_name = 'capture%d_171221'%j
     data.name(data_name)
     try:
-        data.hdf5_write('171220_control_chirp.h5')
+        data.hdf5_write('171221_chirp_control.h5')
         try_again = False
     except:
         print "name taken, trying again..."
