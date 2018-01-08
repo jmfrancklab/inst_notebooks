286170267d278b6b25d5fbfb1635fc0530dc1080
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-01-08 16:30:04 -0500

    updated process chirp to remove zero-voltage sections using contiguous f'n

--- proc_chirp.py
+++ proc_chirp.py
@@ -4,7 +4,7 @@ mixdown = 15e6
 capture_num = 1
 f_axis = linspace(100e3,500e3,100) # must match sweep_frequencies_sqw
 with figlist_var(filename='chirp.pdf') as fl:
-    j=0
+    expno=0
     for date, id_string in [
             ('180104','820_LCs'),
             ('180104','cable'),
@@ -21,7 +21,8 @@ with figlist_var(filename='chirp.pdf') as fl:
         except:
             d = nddata_hdf5(date+'_'+id_string+'.h5/capture1_'+date,
                         directory=getDATADIR(exp_type='test_equip'))
-        if j == 0:
+        d.set_units('t','s')
+        if expno == 0:
             # {{{ CDF the values of the data to see if it's really digitizing with 14 bit
             vals = d.data.flatten()
             myhist = nddata(linspace(-3,3,1000),'val')
@@ -35,27 +36,27 @@ with figlist_var(filename='chirp.pdf') as fl:
             print "number of values along dynamic range",num_values,"log_2 ->",log(num_values)/log(2),"bits"
             fl.plot(myhist, label='Diversity of data matches %0.3f bits'%(log(num_values)/log(2)))
             # }}}
-            fl.next('raw')
         d -= d['t':(0,3.5e-6)].runcopy(mean,'t')
-        if j == 0:
-            fl.plot(d)
-            fl.next('analytic signal, abs')
         d.ft('t',shift=True)
         d = d['t':(0,100e6)] # throw out negative frequencies and low-pass
         d.reorder('ch', first=False) # move ch last
         d.ift('t')
+        if expno == 0:
+            fl.next('analytic signal, abs')
+            fl.plot(abs(d))
+        ranges = abs(d)['ch',0].contiguous(lambda x: x > 0.09*x.data.max())
+        if ranges.shape[0] > 1:
+            raise ValueError("When I try to pull out the waveform, I get more than one range that's greater than 9%")
+        ranges = tuple(ranges[0,:].tolist())
+        d = d['t':ranges]
+        d.setaxis('t', lambda x: x-d.getaxis('t')[0])
         collated_unmixed = d.copy()
         d *= d.fromaxis('t', lambda t: exp(-1j*2*pi*mixdown*t))
-        if j == 0:
-            fl.plot(abs(d))
+        if expno == 0:
             fl.next('analytic signal, phase')
             fl.plot(d.angle)
         fl.next('analytic signal, ratio', legend=True)
         fl.plot(abs(2*d['ch',1]/d['ch',0]), label=id_string)
-        fl.next('ratio, real', legend=True)
-        fl.plot((2*d['ch',1]/d['ch',0]), label=id_string)
-        fl.next('ratio, imag', legend=True)
-        fl.plot((2*d['ch',1]/d['ch',0]).imag, label=id_string)
         fl.next('analytic signal, phase difference', legend=True)
         fl.plot((d['ch',1]/d['ch',0]).angle/pi, '.', label=id_string)
-        j += 1
+        expno += 1
