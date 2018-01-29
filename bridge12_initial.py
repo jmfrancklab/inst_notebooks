5bf6e8e56b8b8c52b86593d9f7380aa857a3380c
Samantha Betts <smbetts@syr.edu>
2018-01-29 12:18:06 -0500

    after getting first email from thorsten, edit the code a bit

--- bridge12_initial.py
+++ bridge12_initial.py
@@ -1,8 +1,16 @@
 from serial.tools.list_ports import comports, grep
 import serial
+import time
+
 portlist = [j.device for j in comports() if j.description==u'Arduino Due Programming Port (COM3)']
 assert len(portlist)==1
 thisport = portlist[0]
 with serial.Serial(thisport, timeout=1) as s:
-    s.write("help\n")
+    time.sleep(20)
+    print "done"
+    s.write("freq 9505.0\r")
+    s.write("help\r")
     print s.read_all()
+    
+   
+
