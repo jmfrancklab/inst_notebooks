f748fb65de7db36703ef6b1c9061db6e6234cce2
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-02-01 19:05:13 -0500

    created findL.py to calculate inductance of coil given resonance f found on scope as user input, created pi_net.py to calculate resonance f and characteristic impedance of a pair of L and C values

---
+++ pi_net.py
@@ -0,0 +1,12 @@
+from pyspecdata import *
+
+L = raw_input ("L in uH: ")
+C = raw_input ("C in pF: ")
+L = float(L)
+C = float(C)
+
+f = sqrt(1./(L*1e-6*C*1e-12))*(1./(2*pi))/1e6
+Z = 1./(f*1e6*2*pi*C*1e-12)
+
+print "Resonance f in MHz: %f"%f
+print "Characteristic impedance in ohm: %f"%Z
