c1c576d04f558ce2f49f19f9313b9160f6fbf8f1
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-03-16 17:47:16 -0400

    Major tweaks of diagnose TL file, now process program only takes one capture and thoroughly separates frequencies between fundamental and harmonics; minimal edit to process pulse

--- diagnose_TL.py
+++ diagnose_TL.py
@@ -4,7 +4,7 @@ import winsound
 #init_logging(level=logging.DEBUG)
 fl = figlist_var()
 
-def process_series(date,id_string):
+def process_series(date,id_string,capture):
     """Process a series of pulse data.
     
     Lumping this as a function so we can do things like divide series, etc.
@@ -27,42 +27,46 @@ def process_series(date,id_string):
     V_pp: nddata
         After using the analytic signal to determine the extent of the pulse, find the min and max.
     """
-    j = 1
-    if j == 1:
-        print "loading signal",j
-        j_str = str(j)
-        d = nddata_hdf5(date+'_'+id_string+'.h5/capture'+j_str+'_'+date,
-                directory=getDATADIR(exp_type='test_equip'))
-        d.set_units('t','s')
-        fl.next('Channel 1, 1')
-        fl.plot(d['ch',0], alpha=0.5, label='label %s'%id_string)
-        d.ft('t',shift=True)
-        fl.next('Raw FT')
-        fl.plot(d['ch',0],alpha=0.15,label='label %s'%id_string)
-        d.ift('t')
-        d.ft('t')
-        d_fundamental = d.copy()
-        d_harmonics = d.copy()
-        d_fundamental = abs(d_fundamental)*abs(d_fundamental)
-        d_fundamental['t':(None,-20e6)]=0
-        d_fundamental['t':(20e6,None)]=0
-        d_harmonics = abs(d_harmonics)*abs(d_harmonics)
-        d_harmonics['t':(-20e6,20e6)]=0
-        d_harmonics['t':(None,-200e6)]=0
-        d_harmonics['t':(200e6,None)]=0
-        print 'integrating '+id_string+' fundamental'
-        fundamental_sum = d_fundamental['ch',0].integrate('t')
-        print fundamental_sum
-        print 'integrating '+id_string+' harmonics'
-        harmonics_sum = d_harmonics['ch',0].integrate('t')
-        print harmonics_sum
+    j = capture 
+    print "loading signal",j
+    j_str = str(j)
+    d = nddata_hdf5(date+'_'+id_string+'.h5/capture'+j_str+'_'+date,
+            directory=getDATADIR(exp_type='test_equip'))
+    d.set_units('t','s')
+    fl.next('Channel 1, 1')
+    fl.plot(d['ch',0], alpha=0.5, label='%s'%id_string)
+    d.ft('t',shift=True)
+    fl.next('Raw FT')
+    fl.plot(d['ch',0],alpha=0.15,label='%s'%id_string)
+    d.ift('t')
+    d.ft('t')
+    d_fundamental = d.copy()
+    d_harmonics = d.copy()
+    d_fundamental = abs(d_fundamental)*abs(d_fundamental)
+    d_fundamental['t':(None,-20e6)]=0
+    d_fundamental['t':(20e6,None)]=0
+    d_harmonics = abs(d_harmonics)*abs(d_harmonics)
+    d_harmonics['t':(-20e6,20e6)]=0
+    d_harmonics['t':(None,-200e6)]=0
+    d_harmonics['t':(200e6,None)]=0
+    print 'integrating '+id_string+' fundamental'
+    fundamental_sum = d_fundamental['ch',0].integrate('t')
+    print fundamental_sum
+    print 'integrating '+id_string+' harmonics'
+    harmonics_sum = d_harmonics['ch',0].integrate('t')
+    print harmonics_sum
     print "done loading all signals for %s"%id_string
+    fl.next('Fundamental FT')
+    fl.plot(d_fundamental['ch',0],alpha=0.3,label='%s'%id_string)
+    fl.next('Harmonics FT')
+    fl.plot(d_harmonics['ch',0],alpha=0.3,label='%s'%id_string)
     return 
 
 for date,id_string in [
        ('180316','duplexer_pi1'),
        ('180316','duplexer_pi2')
         ]:
-    process_series(date,id_string)
+    process_series(date,id_string,capture=1)
+process_series('180315','duplexer_amp1',capture=100)    
 fl.show()
 
