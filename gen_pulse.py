d54fc1bac0888bb428cd2e9cbb28684b51953f3a
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-03-15 23:40:08 -0400

    Important: updated process pulse to plot without basename error (needed a pyspecdata adjustment) and updated generate pulse for high power duplexer tests

--- gen_pulse.py
+++ gen_pulse.py
@@ -52,7 +52,7 @@ def acquire():
         data_name = 'capture%d_180315'%j
         data.name(data_name)
         try:
-            data.hdf5_write('180315_control.h5')
+            data.hdf5_write('180315_test_control_amp1.h5')
             try_again = False
             print "capture number",j
         except:
@@ -89,10 +89,10 @@ def gen_pulse(freq=14.5e6, width=4e-6, ch1_only=True):
         for this_ch in range(1):
             a[this_ch].burst = True
             a.set_burst(per=100e-3) #effectively sets duty cycle (100msec b/w bursts)
-            set_amp = 1
-#           for set_amp in linspace(1,2,2):
-            a[this_ch].ampl=set_amp
-            raw_input("enter")
-            acquire() 
+#            set_amp = 1
+            for set_amp in linspace(25e-3,2.5,10):
+               a[this_ch].ampl=set_amp
+               raw_input("Turn on amp then continue")
+               acquire() 
 gen_pulse()
 
