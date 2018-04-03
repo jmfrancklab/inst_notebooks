572a0a227832e8c8b395c35c34db6da134cfb43c
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-04-03 15:07:45 -0400

    Upated for running bandpass tests

--- proc_chirp.py
+++ proc_chirp.py
@@ -7,7 +7,9 @@ f_axis = linspace(100e3,500e3,100) # must match sweep_frequencies_sqw
 with figlist_var(filename='chirp.pdf') as fl:
     expno=0
     for date, id_string in [
-            ('180329','testLC_Q')
+            ('180329','RLCs_findL'),
+            ('180329','RLCs_findL_2'),
+            ('180329','RLCs_findL_3')
             ]:
         try:
             try:
@@ -48,7 +50,7 @@ with figlist_var(filename='chirp.pdf') as fl:
         if expno == 0:
             fl.next('analytic signal, abs')
             fl.plot(abs(d))
-        ranges = abs(d)['ch',0].contiguous(lambda x: x > 0.2*x.data.max())
+        ranges = abs(d)['ch',1].contiguous(lambda x: x > 0.6*x.data.max())
         ranges = tuple(ranges[0,:].tolist())
         d = d['t':ranges]
         d.setaxis('t', lambda x: x-d.getaxis('t')[0])
@@ -75,11 +77,16 @@ with figlist_var(filename='chirp.pdf') as fl:
         fl.plot(d['ch',0])
         fl.next('which ch1')
         fl.plot(d['ch',1])
+        if expno == 0: 
+            label = '820 pF'
+        if expno == 1:
+            label = '330 pF'
+        if expno == 2:
+            label = '150 pF'       
         fl.next('analytic signal, ratio')
 #        d.setaxis('t', lambda x: x-d.getaxis('t')[0])
-        fl.plot(abs(2*d['ch',0]/d['ch',1]), alpha=0.38)
+        fl.plot(abs(2*d['ch',0]/d['ch',1]), alpha=0.38, label='%s'%label)
         fl.next('analytic signal, phase difference')
 #        d.setaxis('t', lambda x: x-d.getaxis('t')[0])
-        expno_str = str(expno)
-        fl.plot((d['ch',0]/d['ch',1]).angle/pi, '.', alpha=0.2, label='%s'%expno_str)
+        fl.plot((d['ch',0]/d['ch',1]).angle/pi, '.', alpha=0.2, label='%s'%label)
         expno += 1 
