2220f34ea742cf7edee090f7c34338dab3838f95
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-05-02 13:26:52 -0400

    Updates to generate and process chirp file to test bandpass filter (s12, measure Q)

--- chirp.py
+++ chirp.py
@@ -49,7 +49,7 @@ while try_again:
     data_name = 'capture%d'%j
     data.name(data_name)
     try:
-        data.hdf5_write('180501_bandpass_90_220pf.h5')
+        data.hdf5_write('180502_bandpass_duplexer.h5')
         try_again = False
     except Exception as e:
         print e
