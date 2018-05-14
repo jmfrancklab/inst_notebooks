df3beaebea05edf96ec7e148ed6750ce23752db3
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-05-14 18:28:31 -0400

    Updates to pulse generate and process files to test a re-run of the duplexer, this time with components tuned to a slightly higher frequency than before.

--- chirp.py
+++ chirp.py
@@ -49,7 +49,7 @@ while try_again:
     data_name = 'capture%d'%j
     data.name(data_name)
     try:
-        data.hdf5_write('180514_test_M8653x3_8.h5')
+        data.hdf5_write('180514_chirp_low_duplexer_2piTL_low.h5')
         try_again = False
     except Exception as e:
         print e
