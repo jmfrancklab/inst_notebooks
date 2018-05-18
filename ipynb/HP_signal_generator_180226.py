776558b73dbbef462efffbb2da8563634073264d
John Franck (laptop) <jmfranck@syr.edu>
2018-05-18 18:58:04 -0400

    able to detect changes in the power level with the notebook code shown here

--- ipynb/HP_signal_generator_180226.py
+++ ipynb/HP_signal_generator_180226.py
@@ -1,9 +1,15 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[11]:
+
+cd ..
+
+
+# In[12]:
 
 from Instruments import HP8672A
+
 
 # This next block is how I found the correct GPIB address (set a frequency that matches the address)
 
@@ -14,36 +20,38 @@ for j in range(50):
     with hp8672a(gpibaddress=testaddr) as h:
         h.set_frequency(10e9+0.1e9*testaddr)
 
+
 # Note that the manual makes it seem like there might be a different address for receiving info from the instrument, but I also didn't find any info about commands where you can get it to speak to you. 
 
-# In[3]:
+# In[13]:
 
 with HP8672A(gpibaddress=19) as h:
     h.set_frequency(9.2e9)
 
 
-# In[4]:
+# If I have the "RF OUTPUT" switch set to on when I start up the instrument, and then run the following at 9.2 GHz, and see how the HP333308 changes in response, I can see the power changing.
+
+# In[27]:
 
 with HP8672A(gpibaddress=19) as h:
-    h.set_power(-40)
+    h.set_power(-12)
 
+
+# The following is some example socket code from online somewhere -- the StringIO might be useful
 
 # In[ ]:
 
 import socket
 
 
-# In[ ]:
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
 
 
-# In[ ]:
 
 s.connect(('jmfrancklab-prologix.syr.edu',1234))
 
 
-# In[ ]:
 
 s.send('++ver\r')
 import StringIO
@@ -53,11 +61,10 @@ while True:
     buff.write(data)                    # Append that segment to the buffer
     if '\n' in data or '\r' in data: break              # If that segment had '\n', break
 
-# Get the buffer data, split it over newlines, print the first line
-print buff.getvalue().splitlines()[0]
 
+# Get the buffer data, split it over newlines, print the first line
 
 # In[ ]:
 
-
+print buff.getvalue().splitlines()[0]
 
