9b1ec9e4c4679a0a10d33e5385ee20c2a314eef0
John Franck (where?) <jmfranck@syr.edu>
2018-05-23 12:19:24 -0400

    some trouble debugging

--- ipynb/test_noise_180523.py
+++ ipynb/test_noise_180523.py
@@ -5,31 +5,37 @@
 
 get_ipython().magic(u'load_ext pyspecdata.ipy')
 from pyspecdata.fornotebook import standard_noise_comparison, save_data
+init_logging(level='debug')
+logger.debug("a test!")
 
+
+# # Current Summary
+# 
+# I should be able to run the cell that defines ``plot_noise``. In trying to get it to work, I realized that I needed to replace `load_file` (old style) with `find_file`.  For some reason, it's not able to find the file (which is there, I checked with "find" on the command line.)  I need to double-check how far `find_file` will try to recurse.
+# 
+# When I'm done, I should pull all these functions out of pyspecdata, since they are applications.
+
+# # Code
+# pull the following from my notebooks
 
 # In[ ]:
-
-# pull the following from my notebooks
 
 name = 'dna_cs14_120314'
 standard_noise_comparison(name)
 
 
-# In[ ]:
-
 # since this calls on plot_noise (which was deleted when nmr.py was deleted), go back to git and retrieve the plot_noise function
-
 
 # In[ ]:
 
 #{{{ plot_noise
-def plot_noise(path,j,calibration,mask_start,mask_stop,rgmin=0,k_B = None,smoothing = False, both = False, T = 293.0,plottype = 'semilogy',retplot = False):
+def plot_noise(filename,expno,calibration,mask_start,mask_stop,rgmin=0,k_B = None,smoothing = False, both = False, T = 293.0,plottype = 'semilogy',retplot = False, exp_type='shared_drive'):
     '''plot noise scan as resistance'''
-    filename = r'%s%d'%(path,j)
     try:
-        data = load_file(filename,calibration=calibration)
-    except:
-        raise CustomError('error loading file'+filename)
+        data = find_file(filename, expno=expno, calibration=calibration, exp_type=exp_type)
+    except BaseException  as e:
+        raise ValueError("Trouble loading the file "+filename+explain_error(e))
+                         #+ '\n'.join(['>\t'+j for j in str(e).split('\n')]))# this indents
     k_B = 1.3806504e-23
     data.ft('t2',shift = True)
     newt2 = r'F2 / $Hz$'
@@ -82,9 +88,9 @@ def plot_noise(path,j,calibration,mask_start,mask_stop,rgmin=0,k_B = None,smooth
 #}}}
 
 
-# In[2]:
-
 # also, just retrieve the standard_noise_comparison function, since I will probably want to edit it
+
+# In[ ]:
 
 def standard_noise_comparison(name,path = 'franck_cnsi/nmr/', data_subdir = 'reference_data',expnos = [3]):
     print '\n\n'
@@ -154,9 +160,77 @@ def standard_noise_comparison(name,path = 'franck_cnsi/nmr/', data_subdir = 'ref
           print '\n\n'
 
 
+# Here, I want to pull out the potion of ``standard_noise_comparison`` that plots the reference scans
+
 # In[ ]:
 
-get_ipython().magic(u'pinfo save_data')
+figure(1,figsize=(16,8))
+data_subdir = 'reference_data'
+#data_subdir = 'shared_drive' # call with this once, so it finds the reference_data directory
+#{{{ pull the cnsi calibration info from the data text file
+v = save_data()
+our_calibration = double(v['our_calibration'])
+cnsi_calibration = double(v['cnsi_calibration'])
+calibration = cnsi_calibration*sqrt(50.0/10.0)*sqrt(50.0/40.0)
+#}}}
+path_list = []
+explabel = []
+noiseexpno = []
+signalexpno = []
+plotlabel = 'example_noise'
+#
+path_list += ['popem_4mM_5p_pct_110610']
+explabel += ['control without shield']
+noiseexpno += [3] # 3 is the noise scan 2 is the reference
+#
+mask_start = -1e6
+mask_stop = 1e6
+ind = 0
+smoothing = 5e3
+for j in range(0,1): # for multiple plots $\Rightarrow$ add in j index below if this is what i want
+   figure(1)
+   ind += 1
+   legendstr = []
+   linelist = []
+   subplot(121) # so that legend will fit
+   for k in range(0,len(noiseexpno)):
+      retval = plot_noise(path_list[k], noiseexpno[k], calibration, mask_start,
+              mask_stop, smoothing=smoothing,  both=False, retplot=True,
+              exp_type=data_subdir)
+      linelist += retval[0]
+      legendstr.append('\n'.join(textwrap.wrap(explabel[k]+':'+retval[1][0],50))+'\n')
+   ylabel(r'$\Omega$')
+   titlestr = 'Noise scans (smoothed %0.2f $kHz$) for CNSI spectrometer\n'%(smoothing/1e3)
+   title(titlestr+r'$n V$ RG/ disk units = %0.3f, mask (%0.3f,%0.3f)'%(calibration*1e9,mask_start,mask_stop))
+   ax = gca()
+   ylims = list(ax.get_ylim())
+   #gridandtick(gca(),formatonly = True)
+   gridandtick(gca(),logarithmic = True)
+   subplot(122)
+   grid(False)
+   lg = autolegend(linelist,legendstr)
+   ax = gca()
+   ax.get_xaxis().set_visible(False)
+   ax.get_yaxis().set_visible(False)
+   map((lambda x: x.set_visible(False)),ax.spines.values())
+   lplot('noise'+plotlabel+'_%d.pdf'%ind,grid=False,width=5,gensvg=True)
+   print '\n\n'
+   figure(2)
+   legendstr = []
+   for k in range(0,len(signalexpno)):
+      data = load_file(dirformat(path_list[k])+'%d'%noiseexpno[k],calibration=calibration)
+      data.ft('t2',shift = True)
+      x = data.getaxis('t2')
+      data['t2',abs(x)>1e3] = 0
+      data.ift('t2',shift = True)
+      plot(abs(data['t2',0:300])*1e9)
+      xlabel('signal / $nV$')
+      legendstr += [explabel[k]]
+   if len(signalexpno)>0:
+       autolegend(legendstr)
+       lplot('signal'+plotlabel+'_%d.pdf'%ind,grid=False)
+   if (ind % 2) ==  0:
+      print '\n\n'
 
 
 # In[ ]:
