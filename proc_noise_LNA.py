d7d02e3bc234d06ae67bde82c3464d4aec8a949f
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-05-23 18:55:55 -0400

    Updates to processing noise code to plot LNA noise captured at the three diffrent bandwidth settings of the scope (250 MHz, 100 MHz, 20 MHz)

--- proc_noise_LNA.py
+++ proc_noise_LNA.py
@@ -12,7 +12,7 @@ def load_noise(date,id_string,captures):
         print "pulled accumulated data"
     except:
         print "accumulated data was not found, pulling individual captures"
-        for j in xrange(0,cap_len):
+        for j in xrange(1,cap_len+1):
             j_str = str(j)
             d = nddata_hdf5(filename+'/capture'+j_str+'_'+date,
                 directory=getDATADIR(exp_type='test_equip'))
@@ -33,44 +33,50 @@ def load_noise(date,id_string,captures):
 captures = linspace(1,100,100)
 for date,id_string in [
     ('180521','noise_LNA'),
+    ('180523','noise_LNA_bw100'),
+    ('180523','noise_LNA_bw20'),
     ]:
-    s = load_noise(date,id_string,captures)
 
-acq_time = diff(s.getaxis('t')[r_[0,-1]])[0]
-print '\nAcquistion time:',acq_time
-w=1e6 
-plot_y_max = 1
-#Declaring now because cannot set frequency units atm
-s.ft('t',shift=True)
-s_modsq = abs(s)**2     #mod square
-s_modsq /= 519.01901761     #divide by gain factor, found from power curve
-s_modsq /= acq_time         #divide by acquisition time
-s_modsq /= 50.              #divide by resistance, gives units: W*s, or W/Hz
-s_modsq /= k_B*300.         #divide by thermal noise
-s_modsq *= 2 # because the power is split over negative and positive frequencies
-fl.next('without averaging')
-fl.plot(s_modsq.C.convolve('t',w))
-ylim(0,plot_y_max)
-s_avg = s_modsq.C.mean('capture', return_error=False)
-fl.next('Power density($\\nu$)')
-fl.plot(s_avg,alpha=0.3)
-interval = (14.4e6,14.6e6)
-print "integrate from 14.4 to 14.6 MHz (should match P/(k_B T) of injected reference signal):",s_avg['t':interval].integrate('t')
-print "Johnson noise over same interval (also P/(k_B T)) should be:",(interval[1]-interval[0])
-ylim(0,plot_y_max)
-w_str = str(w)
-fl.next('Power densities($\\nu$), width= %.1e Hz'%w)
-fl.plot(s_avg,alpha=0.23,label='PD')
-s_avg = s_avg.convolve('t',w)
-fl.plot(s_avg,alpha=0.2,color='green',label='Convolved')
-ylim(0,plot_y_max)
-fl.next('PD Convolved, width= %.1e Hz'%w)
-fl.plot(s_avg,alpha=0.23)
-ylim(0,plot_y_max)
-fl.next('PD Convolved zoom, width= %.1e Hz'%w)
-fl.plot(s_avg['t':(-600e6,600e6)],alpha=0.23)
-ylim(0,plot_y_max)
-p_J = (abs(s_avg['t':(-600e6,600e6)])).integrate('t')
-print p_J
+    s = load_noise(date,id_string,captures)
+    acq_time = diff(s.getaxis('t')[r_[0,-1]])[0]
+    print '\nAcquistion time:',acq_time
+    w=1e6 
+    plot_y_max = 1
+    #Declaring now because cannot set frequency units atm
+    s.ft('t',shift=True)
+    s_modsq = abs(s)**2     #mod square
+    s_modsq /= 519.01901761     #divide by gain factor, found from power curve
+    s_modsq /= acq_time         #divide by acquisition time
+    s_modsq /= 50.              #divide by resistance, gives units: W*s, or W/Hz
+    #s_modsq /= k_B*300.         #divide by thermal noise
+    #s_modsq *= 2 # because the power is split over negative and positive frequencies
+    s_avg = s_modsq.C.mean('capture', return_error=False)
+    fl.next('power_density($\\nu$)')
+    if id_string == 'noise_LNA':
+        label = 'bandwidth = 250 MHz'
+    if id_string == 'noise_LNA_bw100':
+        label = 'bandwidth = 100 MHz'
+    if id_string == 'noise_LNA_bw20':
+        label = 'bandwidth = 20 MHz'
+    fl.plot(s_avg,alpha=0.3,label="%s"%label)
+    ylim(0,6e-21)
+    #interval = (14.4e6,14.6e6)
+    #print "integrate from 14.4 to 14.6 MHz (should match P/(k_B T) of injected reference signal):",s_avg['t':interval].integrate('t')
+    #print "Johnson noise over same interval (also P/(k_B T)) should be:",(interval[1]-interval[0])
+    ##ylim(0,plot_y_max)
+    ###w_str = str(w)
+    ###fl.next('power_densities($\\nu$),width= %.1e Hz'%w)
+    ###fl.plot(s_avg,alpha=0.23,label='PD')
+    ###s_avg = s_avg.convolve('t',w)
+    ###fl.plot(s_avg,alpha=0.2,color='green',label='Convolved')
+    ###ylim(0,6e-21)
+    ###fl.next('PD Convolved, width= %.1e Hz'%w)
+    ###fl.plot(s_avg,alpha=0.23)
+    ###ylim(0,6e-21)
+    #fl.next('PD Convolved zoom, width= %.1e Hz'%w)
+    #fl.plot(s_avg['t':(-600e6,600e6)],alpha=0.23)
+    ##ylim(0,plot_y_max)
+    #p_J = (abs(s_avg['t':(-600e6,600e6)])).integrate('t')
+    #print p_J
 fl.show()
 
