0ff00a8cb127a8163db809175b659d450f3fcbdd
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-05-23 21:50:04 -0400

    IMPORTANT: Removed on-the-spot averaging during capture of signal in generate pulse code. This is the ideal when measuring incoherent signal (so technically not a pulse). LNA noise processing code contains short program to check integrity of captured voltage vs. scope voltage.

--- proc_noise_LNA.py
+++ proc_noise_LNA.py
@@ -29,12 +29,27 @@ def load_noise(date,id_string,captures):
                 directory=getDATADIR(exp_type='test_equip'))
     return s
 
+def pull_first(date,id_string):
+    filename = date+'_'+id_string+'.h5'
+    j_str = '1'
+    d = nddata_hdf5(filename+'/capture'+j_str+'_'+date,
+        directory=getDATADIR(exp_type='test_equip'))
+    return d['ch',0]
+fl.next('pull first sine dataset to check units')
+d = pull_first('180523','sine_LNA_noavg')
+fl.plot(d)
+d.ft('t', shift=True)
+d['t':(-13e6,13e6)] = 0
+d['t':(16e6,None)] = 0
+d['t':(None,-16e6)] = 0
+d.ift('t')
+fl.plot(d)
+fl.show()
+exit()
 #4096 points
-captures = linspace(1,100,100)
+captures = linspace(0,100,100)
 for date,id_string in [
-    ('180521','noise_LNA'),
-    ('180523','noise_LNA_bw100'),
-    ('180523','noise_LNA_bw20'),
+    ('180523','sine_LNA_noavg'),
     ]:
 
     s = load_noise(date,id_string,captures)
@@ -44,39 +59,47 @@ for date,id_string in [
     plot_y_max = 1
     #Declaring now because cannot set frequency units atm
     s.ft('t',shift=True)
+    T = 293
     s_modsq = abs(s)**2     #mod square
     s_modsq /= 519.01901761     #divide by gain factor, found from power curve
     s_modsq /= acq_time         #divide by acquisition time
     s_modsq /= 50.              #divide by resistance, gives units: W*s, or W/Hz
-    #s_modsq /= k_B*300.         #divide by thermal noise
+    s_modsq /= k_B*T         #divide by thermal noise
     #s_modsq *= 2 # because the power is split over negative and positive frequencies
     s_avg = s_modsq.C.mean('capture', return_error=False)
-    fl.next('power_density($\\nu$)')
     if id_string == 'noise_LNA':
         label = 'bandwidth = 250 MHz'
-    if id_string == 'noise_LNA_bw100':
-        label = 'bandwidth = 100 MHz'
-    if id_string == 'noise_LNA_bw20':
-        label = 'bandwidth = 20 MHz'
-    fl.plot(s_avg,alpha=0.3,label="%s"%label)
-    ylim(0,6e-21)
+    elif id_string == 'sine_LNA':
+        label = '14.5 MHz sine, bandwidth = 250 MHz'
+    else:
+        label = 'undetermined'
+    signal_slice = s_avg['t':(12e6,17e6)]
+    fl.next('power_density($\\nu$)')
+    fl.plot(s_avg,alpha=0.3, label="%s"%label)
+    fl.plot(signal_slice,alpha=0.3, label="signal: %s"%label)
+    fl.next('power_density($\\nu$) semilog')
+    fl.plot(s_avg,alpha=0.3, label="%s"%label, plottype='semilogy')
+    fl.plot(signal_slice,alpha=0.3, label="signal: %s"%label, plottype='semilogy')
+    print "for,",label,"signal is: ",'%0.4e'%(signal_slice.C.integrate('t').data)
+    #ylim(0,6e-21)
     #interval = (14.4e6,14.6e6)
     #print "integrate from 14.4 to 14.6 MHz (should match P/(k_B T) of injected reference signal):",s_avg['t':interval].integrate('t')
     #print "Johnson noise over same interval (also P/(k_B T)) should be:",(interval[1]-interval[0])
-    ##ylim(0,plot_y_max)
+    ###ylim(0,plot_y_max)
     ###w_str = str(w)
     ###fl.next('power_densities($\\nu$),width= %.1e Hz'%w)
     ###fl.plot(s_avg,alpha=0.23,label='PD')
     ###s_avg = s_avg.convolve('t',w)
     ###fl.plot(s_avg,alpha=0.2,color='green',label='Convolved')
-    ###ylim(0,6e-21)
+    ####ylim(0,6e-21)
     ###fl.next('PD Convolved, width= %.1e Hz'%w)
     ###fl.plot(s_avg,alpha=0.23)
-    ###ylim(0,6e-21)
+    ####ylim(0,6e-21)
     #fl.next('PD Convolved zoom, width= %.1e Hz'%w)
     #fl.plot(s_avg['t':(-600e6,600e6)],alpha=0.23)
-    ##ylim(0,plot_y_max)
+    ###ylim(0,plot_y_max)
     #p_J = (abs(s_avg['t':(-600e6,600e6)])).integrate('t')
     #print p_J
 fl.show()
+
 
