91f25df6747ad934c56818b808f79b76d20ca0fa
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-05-30 14:52:44 -0400

    Version of chirp processing file that functions properly in LCR series circuit -- ch1 of scope is full chirp, ch2 of scope is test chirp i.e., reflected chirp

--- proc_chirp.py
+++ proc_chirp.py
@@ -7,19 +7,17 @@ f_axis = linspace(100e3,500e3,100) # must match sweep_frequencies_sqw
 with figlist_var(filename='chirp.pdf') as fl:
     expno=0
     for date, id_string in [
-            ('180515','chirp_duplexer_45mVpp'),
-#            ('180513','test_M8653x3_1'),
-#            ('180513','test_M8653x3_2'),
-#            ('180513','test_M8653x3_3'),
-#            ('180513','test_M8653x3_4'),
-#            ('180514','test_M8653x3_1'),
-            ('180514','test_M8653x3_2'),
-            ('180514','test_M8653x3_3'),
-            ('180514','test_M8653x3_4'),
-#            ('180514','test_M8653x3_5'),
-#            ('180514','test_M8653x3_6'),
-            ('180514','test_M8653x3_7'),
-            ('180514','test_M8653x3_8'),
+#            ('180109','tm2probe'),
+#            ('180109','tm3probe'),
+#            ('180109','tm4probe'),
+#            ('180109','tm4adj1'),
+#            ('180109','tm5probe'),
+#            ('180109','tm6probe'),
+#            ('180109','tm7probe'),
+#            ('180110','tm8probe'),
+#            ('180110','tm9probe')
+            ('180530','chirp_0p5uH'),
+            ('180530','chirp_0p5uH_2')
             ]:
 
         try:
@@ -44,42 +42,21 @@ with figlist_var(filename='chirp.pdf') as fl:
         d = d['t':(0,100e6)] # throw out negative frequencies and low-pass
         d.reorder('ch', first=False) # move ch last
         d.ift('t')
-        if expno == 0:
-            fl.next('analytic signal, abs')
-            fl.plot(abs(d))
-        ranges = abs(d)['ch',1].contiguous(lambda x: x > 0.1*x.data.max())
+        ranges = abs(d)['ch',0].contiguous(lambda x: x > 0.09*x.data.max())
         ranges = tuple(ranges[0,:].tolist())
         d = d['t':ranges]
         d.setaxis('t', lambda x: x-d.getaxis('t')[0])
         d.setaxis('t', lambda x: 25e6-x*25e6/4096e-8).set_units('t','Hz')
         if expno == 0:
-            fl.next('analytic signal, phase')
-            fl.plot(d.angle)
-        d.ft
-        fl.next('FT(chirp)')
-        fl.plot(d['ch',0])
-
-        fl.next('which ch1')
-        fl.plot(d['ch',1])
-
-        if expno == 0:
-            label = 'previous'
-        if expno == 1:
-            label = 'current'
-        if expno == 2:
-            label = '3'       
-#        if expno == 3:
-#            label = '3L,220pF'       
-#        if expno == 4:
-#            label = 'Duplexer (3L bp)'       
-#        if expno == 5:
-#            label = 'Duplexer (2L bp)'       
-        fl.next('chirp')
+            label = id_string
+        fl.next('channel 1')
         fl.plot(d['ch',0],'+',alpha=0.2,label='%s'%label)
-        fl.next('s11: 3xM8653')
+        fl.next('channel 2')
+        fl.plot(d['ch',1],'+',alpha=0.2,label='%s'%label)
+        fl.next('s11: analytic')
 #        d.setaxis('t', lambda x: x-d.getaxis('t')[0])
-        fl.plot(2*abs(d['ch',0]/d['ch',1]),'-', alpha=0.38, label='%s'%label)
-        fl.next('bandpass s12: phase')
+        fl.plot(2*abs(d['ch',1]/d['ch',0]),'-', alpha=0.38, label='%s'%label)
+        fl.next('s11: phase')
 #        d.setaxis('t', lambda x: x-d.getaxis('t')[0])
-        fl.plot((d['ch',0]/d['ch',1]).angle/pi, '.', alpha=0.2, label='%s'%label)
+        fl.plot((d['ch',1]/d['ch',0]).angle/pi, '.', alpha=0.2, label='%s'%label)
         expno += 1 
