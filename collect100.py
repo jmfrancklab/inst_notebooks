05933d92433bb7b405aa1157a3b9bd6085f025b4
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-06-08 14:44:54 -0400

    Update to collection file, which is for collecting large number of unaveraged captures on scope (for noise tests or signal tests for generating PSD), that removes the naming loop and significantly reduces run time of program.

--- collect100.py
+++ collect100.py
@@ -5,10 +5,7 @@ from serial.tools.list_ports import comports
 import serial
 from scipy import signal
 
-#raw_input("Warning --> Detach from amplifier before proceeding further (set AFG before hooking up, since it generates cw)")
-
 acquire = False
-
 fl = figlist_var()
 
 print "These are the instruments available:"
@@ -18,17 +15,15 @@ print "done printing available instruments"
 with SerialInstrument('GDS-3254') as s:
     print s.respond('*idn?')
     
-def acquire():
+def acquire(x):
     datalist = []
     print "about to load GDS"
     with GDS_scope() as g:
-    #    g.timscal(5e-6)  #setting time scale to 500 ns/div
-    #    g.voltscal(1,500e-3) #setting volt scale on channel 1 to 500 mV/div
         print "loaded GDS"
         ch1_waveform = g.waveform(ch=1)
         ch2_waveform = g.waveform(ch=2)
     data = concat([ch1_waveform,ch2_waveform],'ch').reorder('t')
-    # {{{ in case it pulled from an inactive channel
+    #{{{ in case pulled from inactive channel
     if not isfinite(data.getaxis('t')[0]):
         j = 0
         while not isfinite(data.getaxis('t')[0]):
@@ -36,56 +31,23 @@ def acquire():
             j+=1
             if j == len(datalist):
                 raise ValueError("None of the time axes returned by the scope are finite, which probably means no traces are active??")
-    # }}}
-    j = 1
-    try_again = True
-    while try_again:
-        data_name = 'capture%d_180606'%j
-        data.name(data_name)
-        try:
-            data.hdf5_write('180606_sine_LNA1_2CH.h5')
-            try_again = False
-            print "capture number",j
-        except:
-            #print "name taken, trying again..."
-            j += 1
-            try_again = True
+    #}}}
+    data_name = 'capture%d_180608'%x
+    data.name(data_name)
+    data.hdf5_write('180608_testing4_LNA1.h5')
+    print "capture number",x
     print "name of data",data.name()
     print "units should be",data.get_units('t')
     print "shape of data",ndshape(data)
-    fl.next('Dual-channel data')
-    fl.plot(data)
-
+    return
 
 def gen_pulse(freq=14.5e6, width=4e-6, ch1_only=True):
-#    with AFG() as a:
-#        a.reset()
-#        rate = freq*4
-#        total_samples = width*rate
-#        total_samples = int(total_samples/4 + 0.5)*4 # convert to multiple of 4
-#        assert total_samples < 4097, "Your pattern length (%d) exceeds the max (4096 samples at %g MHz)"%(total_samples,rate)
-#        y = zeros(total_samples)
-#        y[0::4]=0
-#        y[1::4]=1
-#        y[2::4]=0
-#        y[3::4]=-1
-#        y[-1]=0
-#        if ch1_only:
-#            ch_list = [0]
-#        else:
-#            ch_list = [0,1]
-#        for this_ch in ch_list:
-#            a[this_ch].digital_ndarray(y, rate=rate)
-#            print "now, output on"
-#            a[this_ch].output = True
-#        for this_ch in range(1):
-#            a[this_ch].burst = True
-#            a.set_burst(per=100e-3) #effectively sets duty cycle (100msec b/w bursts)
-#            set_amp = 50e-3
-#            a[this_ch].ampl = set_amp
-    raw_input("Set")
-    for x in linspace(0,100,100): 
-       print "capture",x 
-       acquire() 
+    raw_input("Confirm Sample Rate = 2.5 GSPS")
+    for x in xrange(0,101):
+        print "entering capture",x
+        acquire(x)
+    return
+
+
 gen_pulse()
 
