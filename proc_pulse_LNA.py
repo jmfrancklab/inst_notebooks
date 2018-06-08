0b90cb2960f2f6f28d504e7e3e3a31a4de7e99fe
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-06-07 21:22:35 -0400

    Merged RMS and PP processing files (proc_pulse_RMS and proc_pulse_LNA, respectively) into one, proc_pulse_LNA. Heavily based off previous modifications to proc_pulse. Importantly produces an output vs input power dataset that uses a reference signal on the x-axis, therefore processes 2 channels instead of just 1 (more appropriate name may be proc_pulse_2ch)

--- proc_pulse_LNA.py
+++ proc_pulse_LNA.py
@@ -1,6 +1,8 @@
 from pyspecdata import *
 fl = figlist_var()
-
+import os
+import sys
+#{{{ Functions for plotting different captures of one data set
 def gen_plotdict(capture_list,plot_str):
     plotdict = {}
     for n in xrange(len(capture_list)):
@@ -18,148 +20,207 @@ def plot_captures(capture_list,plot_str,current_j,data,how_many_ch):
                 k = ch_no + 1
                 fl.plot(data['ch',ch_no],alpha=0.2,label='CH%d'%k)
     return
+#}}}
+#{{{ Choose parameter input (script or user) 
+params_choice = int(sys.argv[1])
+if params_choice == 0:
+    print "Choosing script-defined parameters..."
+    print "(make sure parameters are what you want)\n"
+    V_start = 0.01
+    V_stop = 0.01321941
+    V_step = 5
+    V_start_log = log10(V_start)
+    V_stop_log = log10(V_stop)
+    V_step_log = V_step
+    V_AFG = logspace(V_start_log,V_stop_log,V_step)
+    print "V_AFG(log10(%f),log10(%f),%f)"%(V_start,V_stop,V_step)
+    print "V_AFG(%f,%f,%f)"%(log10(V_start),log10(V_stop),V_step)
 
-def gen_power_data(date,id_string,V_AFG,pulse_threshold):
+    rms_method=False
+    if rms_method:
+        method = 'RMS'
+    if not rms_method:
+        method = 'PP'
+    print "*** Processing method:",method,"***\n"
+elif params_choice == 1:
+    print "Requesting user input..."
+    V_start = raw_input("Input start of sweep in Vpp: ")
+    V_start = float(V_start)
+    print V_start
+    V_stop = raw_input("Input stop of sweep in Vpp: ")
+    V_stop = float(V_stop)
+    print V_stop
+    V_step = raw_input("Input number of steps: ")
+    V_step = float(V_step)
+    print V_step
+
+    axis_spacing = raw_input("1 for log scale, 0 for linear scale: ")
+    if axis_spacing == '1':
+        V_start_log = log10(V_start)
+        V_stop_log = log10(V_stop)
+        V_AFG = logspace(V_start_log,V_stop_log,V_step)
+        print "V_AFG(log10(%f),log10(%f),%f)"%(V_start,V_stop,V_step)
+        print "V_AFG(%f,%f,%f)"%(log10(V_start),log10(V_stop),V_step)
+        print V_AFG
+    elif axis_spacing == '0':
+        V_AFG = linspace(V_start,V_stop,V_step)
+        print "V_AFG(%f,%f,%f)"%(V_start,V_stop,V_step)
+        print V_AFG
+    
+    method_choice = raw_input("1 for PP method, 0 for RMS method: ")
+    if method_choice == '1':
+        rms_method = False
+    elif method_choice == '0':
+        rms_method = True
+    if rms_method:
+        method = 'RMS'
+    if not rms_method:
+        method = 'PP'
+    print "*** Processing method:",method,"***\n"
+        # }}}
+# {{{ Signal processing function
+def gen_power_data(date, id_string, V_AFG, rms_method,
+        pulse_threshold=0.5):
+    """Process a series of pulse data to produce output vs input power set
+    ready for plotting.
+    
+    Lumping this as a function so we can do things like divide series, etc.
+
+    Parameters
+    ----------
+    date: str
+        date used in the filename
+    id_string: str
+        filename is called date_id_string
+    V_AFG: array
+        list of voltage settings used on the AFG 
+    rms_method: boolean
+        if True, calls rms_signal_to_power() which returns power of input data set 
+        by subtracting V_RMS noise from V_RMS pulse and converting the difference to
+        power.
+        if False, calls pp_signal_to_power() which returns power of input data set
+        by subtracting minimum V from maximum V within the pulse length, and converting
+        the difference (V_PP) to power. Original method used for processing such data 
+        sets until 06/2018.
+    pulse_threshold: float 
+        determines cut-off limit for calculating pulse power;
+        this number is multiplied by the maximum V_{RMS} in the pulse width.
+
+    Returns
+    -------
+    return_power : nddata
+        output power (y-axis) vs input power (x-axis) for each capture
+    """
+    #{{{ RMS function: calculates power by subtracting RMS noise from RMS signal, both analytic
+    def rms_signal_to_power(data,ch):
+        "find the pulse, calculate the noise and signal powers and subtract them"
+        pulse0_slice = abs(data['power',-1]).contiguous(lambda x: x>pulse_threshold*x.data.max())
+        pulse0_slice = pulse0_slice[0,:]
+        pulse_limits = pulse0_slice + r_[0.6e-6,-0.6e-6]
+        noise_limits = pulse0_slice - r_[0.6e-6,-0.6e-6]
+        p0_lim1,p0_lim2 = pulse_limits
+        n0_lim1,n0_lim2 = noise_limits
+        Vn1_rms0 = sqrt((abs(data['t':(0,n0_lim1)]
+            )**2).mean('t',return_error=False))
+        Vn2_rms0 = sqrt((abs(data['t':(0,n0_lim2)]
+            )**2).mean('t',return_error=False))
+        Vn_rms0 = (Vn1_rms0 + Vn2_rms0)/2.
+        V_rms0 = sqrt((abs(data['t':tuple(pulse0_slice)]
+            )**2).mean('t',return_error=False))
+        V_rms0 -= Vn_rms0
+        print 'CH',ch,'Noise limits in microsec: %f, %f'%(n0_lim1/1e-6,n0_lim2/1e-6)
+        print 'CH',ch,'Pulse limits in microsec: %f, %f'%(p0_lim1/1e-6,p0_lim2/1e-6)
+        return (V_rms0)**2./50.
+    #}}}
+    #{{{ PP method: calculates power by subtract min from max analytic signal within pulse slice
+    def pp_signal_to_power(data,ch):
+        pulse_max = abs(data['power',-1].data.max())
+        pulse_slice = abs(data['power',-1]).contiguous(lambda x: x>pulse_threshold*x.data.max())
+        pulse_slice = pulse_slice[0,:]  
+        pulse_slice += r_[0.6e-6,-0.6e-6]
+        pulse_limits = tuple(pulse_slice)
+        p_lim1,p_lim2 = pulse_limits
+        print 'CH',ch,'Pulse limits in microsec: %f, %f'%(p_lim1/1e-6,p_lim2/1e-6)
+        #Important point: Should this be modified to make it so that we are calculate V_pp from the
+        #   raw data and not the analytic signal, as we had this method originally? 
+        V_pp = data['t':tuple(pulse_slice)].run(max,'t')
+        V_pp -= data['t':tuple(pulse_slice)].run(min,'t')
+        return (V_pp)**2./50. 
+    #}}}    
     p_len = len(V_AFG)
-    for j in range(1,p_len+1):
-        print "loading signal",j
-        j_str = str(j)
-        d = nddata_hdf5(date+'_'+id_string+'.h5/capture'+j_str+'_'+date,
+    V_calib = 0.694*V_AFG
+    filename = date+'_'+id_string+'.h5'
+    try:
+        analytic_signal = nddata_hdf5(filename+'/accumulated_'+date,
                 directory=getDATADIR(exp_type='test_equip'))
-        d.set_units('t','s')
-        if j == 1:
-            raw_signal = (ndshape(d) + ('power',p_len)).alloc()
-            raw_signal.setaxis('t',d.getaxis('t')).set_units('t','s')
-            raw_signal.setaxis('power',(V_AFG/2.0/sqrt(2))**2/50.).set_units('power','W')
-        raw_signal['power',j-1]=d
-        capture_list = [1,p_len]
-        plot_captures(capture_list,'RAW',j,d,2)
-        d.ft('t',shift=True)
-        plot_captures(capture_list,'RAW FT',j,abs(d),2)
-        d = d['t':(0,None)]
-        d['t':(0,5e6)] = 0
-        d['t':(25e6,None)] = 0
-        plot_captures(capture_list,'ANALYTIC FT',j,abs(d),2)
-        d.ift('t')
-        d *= 2
-        plot_captures(capture_list,'ANALYTIC',j,abs(d),2)
-        #I only want to do the following for d['ch',0] (control, ch1)3
-        #NOT d['ch',1], since I want to define the power axis values for ch2
-        #as values I generate for ch1
-        if j == 1:
-            analytic_signal = (ndshape(d) + ('power',p_len)).alloc()
-            analytic_signal.setaxis('t',d.getaxis('t')).set_units('t','s')
-            #Do I need to do this? Isn't it arbitrary?
-            analytic_signal.setaxis('power',(V_AFG/2/sqrt(2))**2/50.)
-        analytic_signal['power',j-1]=d
-    print "Done loading signal for %s \n\n"%id_string 
-    pulse0_max = abs(analytic_signal['ch',0]['power',-1].data.max())
-    pulse0_slice = abs(analytic_signal['ch',0]['power',-1]).contiguous(lambda x: x>pulse_threshold*x.data.max())
-    pulse0_slice = pulse0_slice[0,:] #######What are the next 2 lines doing? 
-    pulse0_slice += r_[0.6e-6,-0.6e-6]
-    pulse0_limits = tuple(pulse0_slice)
-    p0_lim1,p0_lim2 = pulse0_limits
-    print 'Control pulse limits in microsec: %f, %f'%(p0_lim1/1e-6,p0_lim2/1e-6)
-    V_pp0 = raw_signal['ch',0]['t':tuple(pulse0_slice)].run(max,'t')
-    V_pp0 -= raw_signal['ch',0]['t':tuple(pulse0_slice)].run(min,'t')
-    #Using exact attenuation of VY574681722
-    VinPP = 500.e-3
-    VoutPP = 4.20e-3
-    VinRMS = (VinPP)/(2*sqrt(2))
-    VoutRMS = (VoutPP)/(2*sqrt(2))
-    Pin_ = ((VinRMS)**2)/50.
-    Pout_ = ((VoutRMS)**2)/50.
-    atten_factor_P = (Pout_)/(Pin_)
-    power0 = ((V_pp0/2./sqrt(2.))**2./50.)*atten_factor_P
-    dBm0 = 10*log10(power0/1e-3)
-    print power0.data
-    s = analytic_signal['ch',1]
-    s.rename('power','power_in').setaxis('power_in',power0.data).set_units('power_in','W')
-    print s
-    pulse1_max = abs(s['power_in',-1].data.max())
-
-    pulse1_slice = abs(s['power_in',-1]).contiguous(lambda x: x>pulse_threshold*x.data.max())
-    pulse1_slice = pulse1_slice[0,:]
-    pulse1_slice += r_[0.6e-6,-0.6e-6]
-    pulse1_limits = tuple(pulse1_slice) 
-    p1_lim1,p1_lim2 = pulse1_limits
-    print 'Amplifier pulse limits in microsec: %f, %f'%(p1_lim1/1e-6,p1_lim2/1e-6)
-    V_pp1 = raw_signal['ch',1]['t':tuple(pulse1_slice)].run(max,'t')
-    V_pp1 -= raw_signal['ch',1]['t':tuple(pulse1_slice)].run(min,'t')
-    power1 = (V_pp1/2./sqrt(2.))**2./50.
-    power_amp = power1
-    power_amp.name('$P_{out}$').set_units('W')  #***IMPORTANT!***
-    power_amp.rename('power','$P_{in}$').setaxis('$P_{in}$',power0.data).set_units('$P_{in}$','W')
-    print power_amp
-    return power_amp
-
-## NO USER INPUT; LOG SPACING
-V_start = 0.01
-V_stop = 0.860
-V_step = 40 
-V_start_log = log10(V_start)
-V_stop_log = log10(V_stop)
-V_step_log = log10(V_step)
-V_AFG = logspace(V_start_log,V_stop_log,V_step)
-print "V_AFG(log10(%f),log10(%f),%f)"%(V_start,V_stop,V_step)
-print "V_AFG(%f,%f,%f)"%(log10(V_start),log10(V_stop),V_step)
-#Ignore attenuation here, does not correspond to the corrections we apply later
-atten_p = 1
-atten_V = 1
-#print "power, Voltage attenuation factors = %f, %f"%(atten_p,atten_V) 
-#print "Axis spacing: Log"
-
+        analytic_signal.set_units('t','s')
+    except:
+        print "accumulated data was not found, pulling individual captures"
+        for j in xrange(1,p_len+1):
+            print "loading signal",j
+            j_str = str(j)
+            d = nddata_hdf5(filename+'/capture'+j_str+'_'+date,
+                    directory=getDATADIR(exp_type='test_equip'))
+            d.set_units('t','s')
+            if j == 1:
+                raw_signal = (ndshape(d) + ('power',p_len)).alloc()
+                raw_signal.setaxis('t',d.getaxis('t')).set_units('t','s')
+                raw_signal.setaxis('power',((V_calib/2.0/sqrt(2))**2/50.)).set_units('power','W')
+            raw_signal['power',j-1] = d
+            d.ft('t',shift=True)
+            d = d['t':(0,None)]
+            d['t':(0,5e6)] = 0
+            d['t':(25e6,None)] = 0
+            d.ift('t')
+            d *= 2
+            if j == 1:
+                analytic_signal = (ndshape(d) + ('power',p_len)).alloc()
+                analytic_signal.setaxis('t',d.getaxis('t')).set_units('t','s')
+                analytic_signal.setaxis('power',(V_calib/2/sqrt(2))**2/50.)
+            analytic_signal['power',j-1] = d
+        analytic_signal.name('accumulated_'+date)
+        analytic_signal.labels('ch',r_[1,2])
+        analytic_signal.hdf5_write(filename,
+                directory=getDATADIR(exp_type='test_equip'))
+    fl.next('Highest power, analytic')
+    fl.plot(abs(analytic_signal['ch',0]['power',-1]),label=id_string)
+    fl.plot(abs(analytic_signal['ch',1]['power',-1]),label=id_string)
+    highest_power = abs(analytic_signal['power',-1])
+    for ch in xrange(0,2):
+        this_data = highest_power['ch',ch]
+        pulse0_slice = this_data.contiguous(lambda x: x>pulse_threshold*x.data.max())
+        pulse0_slice = tuple(pulse0_slice[0,:])
+        fl.plot(this_data['t':pulse0_slice]['t',r_[0,-1]],'o',label='CH%s'%str(ch))
+    print "DATA: %s"%id_string 
+    #{{{ NOTE: ASSUMES CH1=REFERENCE AND CH2=DUT
+    if rms_method:
+        power0 = rms_signal_to_power(analytic_signal['ch',0],ch=1)
+        power0 *= atten_factor
+        power1 = rms_signal_to_power(analytic_signal['ch',1],ch=2)
+    if not rms_method:
+        power0 = abs(pp_signal_to_power(analytic_signal['ch',0],ch=1))
+        power0 *= atten_factor
+        power1 = abs(pp_signal_to_power(analytic_signal['ch',1],ch=2))
+        #}}}
+    return_power = power1
+    return_power.name('$P_{out}$')#.set_units('W')
+    return_power.rename('power','$P_{in}$').setaxis('$P_{in}$',power0.data)#.set_units('$P_{in}$','W')
+    return return_power
+#}}}
+atten_factor = 7.056e-5
 for date,id_string in [
-#        ('180601','sweep_pomona_dpx_cascade12'),
-        ('180528','sweep_cascade12'),
-#        ('180526','sweep_test_LNA1'),
-#        ('180526','sweep_test_LNA2'),
-#        ('180526','sweep_test_LNA3'),
+        ('180607','sweep_cascade12'),
+        ('180526','sweep_test_LNA1'),
+        ('180526','sweep_test_LNA2'),
+        ('180526','sweep_test_LNA3'),
         ]:
-    LNA_power = gen_power_data(date,id_string,V_AFG,pulse_threshold=0.1)
-
-fl.next('Power Curve: LNA #3, PP processing')
-fl.plot(LNA_power,'.',plottype='loglog')
-print LNA_power
-c, result = LNA_power.polyfit('$P_{in}$',force_y_intercept=0)
-fl.plot(result,plottype='loglog')
-print "\n \n \nprinting results for ",id_string
-print c
+    power_plot = gen_power_data(date,id_string,V_AFG,rms_method)
+#    truncate_LNAp = LNA_power['$P_{in}$':((1e2*1e-12),None)]
+    fl.next('Power Curves (%s method)'%method)
+    fl.plot(power_plot,'.',label=date+id_string,plottype='loglog',)
+    c,result = power_plot.polyfit('$P_{in}$',force_y_intercept=0)
+    fl.plot(result,label='linear fit',plottype='loglog')
+    print "Fit parameters for",id_string
+    print c,'\n'
 fl.show()
-
-## REQUESTS USER INPUT
-#V_start = raw_input("Input start of sweep in Vpp: ")
-#V_start = float(V_start)
-#print V_start
-#V_stop = raw_input("Input stop of sweep in Vpp: ")
-#V_stop = float(V_stop)
-#print V_stop
-#V_step = raw_input("Input number of steps: ")
-#V_step = float(V_step)
-#print V_step
-#
-#axis_spacing = raw_input("1 for log scale, 0 for linear scale: ")
-#if axis_spacing == '1':
-#    V_start_log = log10(V_start)
-#    V_stop_log = log10(V_stop)
-#    V_AFG = logspace(V_start_log,V_stop_log,V_step)
-#    print "V_AFG(log10(%f),log10(%f),%f)"%(V_start,V_stop,V_step)
-#    print "V_AFG(%f,%f,%f)"%(log10(V_start),log10(V_stop),V_step)
-#    print V_AFG
-#elif axis_spacing == '0':
-#    V_AFG = linspace(V_start,V_stop,V_step)
-
-#    print "V_AFG(%f,%f,%f)"%(V_start,V_stop,V_step)
-#    print V_AFG
-#
-#atten_choice = raw_input("1 for attenuation, 0 for no attenuation: ")
-#if atten_choice == '1':
-#    atten_p = 10**(-40./10.)
-#    atten_V = 10**(-40./20.)
-#elif atten_choice == '0':
-#    atten_p = 1
-#    atten_V = 1
-#print "power, Voltage attenuation factors = %f, %f"%(atten_p,atten_V) 
-
-
 
