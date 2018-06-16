36450bbf36513ac778ce91c443c98511505a6892
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-06-16 19:27:43 -0400

    Updates to generate chirp file to set a corrected voltage of reference channel based on set voltage of DUT so that each chirp is displayed at the same amplitude on the scope; there is a corresponding edit to the processing file that removes the doubling of the DUT signal which was the previous method of dealing with this issue. Processing file remains compatible with previous collections via boolean statement specified upon file call.

--- chirp.py
+++ chirp.py
@@ -21,23 +21,35 @@ with AFG() as a:
     a.reset()
     t = r_[0:4096]
     y = imag(exp(1j*2*pi*0.25*(1-0.5/4096.*t)*t))
-    set_amp =3. 
-    set
+#    ref_amp = 0.25 
+    ref_amp = 0.75
+    DUT_amp = sqrt(((((ref_amp/2/sqrt(2))**2)/50)/4)*50)*2*sqrt(2)
     for this_ch in range(2):
         a[this_ch].digital_ndarray(y,rate=100e6)
-        print "now, output on"
         a[this_ch].output = True
     for this_ch in range(2):
         a[this_ch].burst = True
-        a[this_ch].ampl=set_amp
+        #setting ref channel to amplitude after power splitter
+        if this_ch == 0: 
+            a[this_ch].ampl=DUT_amp
+        #setting DUT channel to amplitude before power splitter
+        elif this_ch == 1: 
+            a[this_ch].ampl=ref_amp
+        else:
+            print "Channel not recognized"
 
 datalist = []
 print "about to load GDS"
 #raw_input("Turn on RF amp") 
 
 with GDS_scope() as g:
-#     g.timscal(500e-9)  #setting time scale to 500 ns/div
-#     g.voltscal(1,500e-3) #setting volt scale on channel 1 to 500 mV/div
+    g.timscal(5e-6)  #setting time scale to 5 micros/div
+    if ref_amp == 0.25:
+        for this_ch in range(2):
+            g[this_ch].voltscal = 20e-3 #setting volt scale to 20 mV/div
+    elif ref_amp == 0.75:
+        for this_ch in range(2):
+            g[this_ch].voltscal = 50e-3 #setting volt scale to 50 mV/div
     print "loaded GDS"
     for j in range(1,3):
         print "trying to grab data from channel",j
@@ -50,7 +62,7 @@ while try_again:
     data_name = 'capture%d'%j
     data.name(data_name)
     try:
-        data.hdf5_write('180615_chirp_pi.h5')
+        data.hdf5_write('180616_chirp_test.h5')
         try_again = False
     except Exception as e:
         print e
