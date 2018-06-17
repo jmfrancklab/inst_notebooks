0130b6e7597123789666d162c94263caa08f9b94
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-06-16 23:21:27 -0400

    Chirps for control measurements of Sinkovits design

--- chirp.py
+++ chirp.py
@@ -95,7 +95,7 @@ while try_again:
     data_name = 'capture%d'%j
     data.name(data_name)
     try:
-        data.hdf5_write('180616_chirp_test5.h5')
+        data.hdf5_write('180616_chirp_control_short.h5')
         try_again = False
     except Exception as e:
         print e
