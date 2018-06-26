ccbc27f1e01c5e1407afbc9feb757e3e9afdc352
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-06-25 20:16:08 -0400

    Renamed noise processing file from "proc_noise_LNA.py" to "proc_noise_2ch.py"

---
+++ proc_noise_2ch.py
@@ -0,0 +1,252 @@
+from pyspecdata import *
+fl = figlist_var()
+import os
+import sys
+#4096 points
+# {{{ constants measured elsewhere
+gain_factor_amp1 = 525.94786172         #LNA 2
+gain_factor_amp2 = 531.84920761         #LNA 1
+gain_factor_casc12 = 171428.95568926    #cascade (1 then 2)
+gain_factor_damp1 = 318.5103874         #duplexer,LNA 1 
+gain_factor_damp2 = 325.65682308        #duplexer,LNA 2 
+gain_factor_dcasc12 = 114008.55204672   #duplexer,cascade(1,2)
+gain_factor_pdcasc12 = 45514.53212012    #probe,duplexer,cascade
+
+#gain_factor_amp1 = 521.27172202                         #LNA1 gain factor
+#gain_factor_amp2 = 529.98023528                        #LNA2 gain factor
+#gain_factor_amp3 = 523.73589899                        #LNA3 gain factor
+##gain_factor_casc12_ = 203383.76725939914
+##gain_factor_casc12 = 174549.75561175 
+#gain_factor_casc12 = 193879.86939256                  #LNA1,LNA2 gain factor
+#gain_factor_dpx_ = 0.6141065062411988
+#gain_factor_dpx = 0.7156772659294433                  #Duplexer gain factor
+##gain_factor_tot = 113984.00001949
+#gain_factor_tot = 120618.95681133 
+scope_noise = 4.4578468934e-19                         # pulled from the gain=1.0 calculation of the
+                                                        # scope noise, below
+atten_factor = 7.056e-5
+T = 273.15 + 20.
+power_signal_AFG = ((50.e-3)/(sqrt(2)*2))**2./50.
+test_signal_power = power_signal_AFG * atten_factor
+# }}}
+    # {{{ Command line arguments for integration interval 
+default = True
+try:
+    sys.argv[1]
+    default = False
+except:
+    sys.argv[0]
+if default:
+    integration = False
+if not default:
+    integration = True
+    width_choice = int(sys.argv[1])
+    if width_choice == 1:
+        integration_center = 1.45e7
+        integration_width = 2.2e6
+    elif width_choice == 2:
+        integration_center = 1.452e7
+        integration_width = 2.42e5
+    elif width_choice == 3:
+        integration_center = 14.5e6 
+        integration_width = 5.7e6
+    elif width_choice == 4:
+        integration_center = 1.45e7
+        integration_width = 6.28e6
+    elif width_choice == 5:
+        integration_center = 1.45e7
+        integration_width = 8.11e5
+    elif width_choice == 20:
+        integration_center = 20.e6 
+        integration_width = 6.4e6
+    elif width_choice == 25:
+        integration_center = 25.e6 
+        integration_width = 6.3e6
+    elif width_choice == 43:
+        integration_center = 43.47655e6 
+        integration_width = 15.14685e6
+    elif width_choice == 55:
+        integration_center = 55.e6 
+        integration_width = 10.e6
+    else:
+        print "Unrecognized width choice"
+        integration_center = 14.5e6 
+        integration_width = 2.e6
+    # }}}
+
+def load_noise(date,id_string,captures):
+    cap_len = len(captures)
+    filename = date+'_'+id_string+'.h5'
+    try:
+        s = nddata_hdf5(filename+'/accumulated_'+date,
+                directory=getDATADIR(exp_type='test_equip'))
+        s.set_units('t','s')
+    except:
+        print "accumulated data was not found, pulling individual captures"
+        for j in xrange(1,cap_len+1):
+            j_str = str(j)
+            d = nddata_hdf5(filename+'/capture'+j_str+'_'+date,
+                directory=getDATADIR(exp_type='test_equip'))
+            d.set_units('t','s')
+            if j == 1:
+                channels = ((ndshape(d)) + ('capture',cap_len)).alloc()
+                channels.setaxis('t',d.getaxis('t')).set_units('t','s')
+                channels.setaxis('ch',d.getaxis('ch'))
+            print "loading signal %s into capture %s "%(j_str,j_str)
+            channels['capture',j-1]=d        
+        s = channels
+        s.labels('capture',captures)
+        s.name('accumulated_'+date)
+        s.hdf5_write(filename,
+                directory=getDATADIR(exp_type='test_equip'))
+    return s
+
+captures = linspace(0,100,100)
+power_dens_CH1_dict = {}
+power_dens_CH2_dict = {}
+
+# {{{ call files
+for date,id_string,numchan,gain_factor in [
+        ('180625','network_no_diodes_2p5G',2,gain_factor_dcasc12),
+        ('180625','network_no_diodes_1G',2,gain_factor_dcasc12),
+        ('180625','network_no_diodes_500M',2,gain_factor_dcasc12),
+        ('180625','network_no_diodes_250M',2,gain_factor_dcasc12),
+        ('180625','network_no_diodes_100M',2,gain_factor_dcasc12),
+#        ('180625','network_2p5G',2,gain_factor_dcasc12),
+#        ('180625','network_1G',2,gain_factor_dcasc12),
+#        ('180625','network_500M',2,gain_factor_dcasc12),
+#        ('180625','network_250M',2,gain_factor_dcasc12),
+#        ('180625','network_100M',2,gain_factor_dcasc12),
+#        ('180625','network_22MHz_2p5G',2,gain_factor_dcasc12),
+#        ('180625','network_22MHz_1G',2,gain_factor_dcasc12),
+#        ('180625','network_22MHz_500M',2,gain_factor_dcasc12),
+#        ('180625','network_22MHz_250M',2,gain_factor_dcasc12),
+#        ('180625','network_22MHz_100M',2,gain_factor_dcasc12),
+#        ('180625','network_22MHz_no_diodes_2p5G',2,gain_factor_dcasc12),
+#        ('180625','network_22MHz_no_diodes_1G',2,gain_factor_dcasc12),
+#        ('180625','network_22MHz_no_diodes_500M',2,gain_factor_dcasc12),
+#        ('180625','network_22MHz_no_diodes_250M',2,gain_factor_dcasc12),
+#        ('180625','network_22MHz_no_diodes_100M',2,gain_factor_dcasc12),
+#        ('180625','network_22MHz_2p5G_2',2,gain_factor_dcasc12),
+#        ('180625','network_22MHz_1G_2',2,gain_factor_dcasc12),
+#        ('180625','network_22MHz_500M_2',2,gain_factor_dcasc12),
+#        ('180625','network_22MHz_250M_2',2,gain_factor_dcasc12),
+#        ('180625','network_22MHz_100M_2',2,gain_factor_dcasc12),
+#        ('180625','network_22MHz_no_diodes_2p5G_2',2,gain_factor_dcasc12),
+#        ('180625','network_22MHz_no_diodes_1G_2',2,gain_factor_dcasc12),
+#        ('180625','network_22MHz_no_diodes_500M_2',2,gain_factor_dcasc12),
+#        ('180625','network_22MHz_no_diodes_250M_2',2,gain_factor_dcasc12),
+#        ('180625','network_22MHz_no_diodes_100M_2',2,gain_factor_dcasc12),
+#    ('180526','AFG_terminator_2',2,1.0),#   leave gain set to 1 so we can get the 
+                                         #   absolute number here (not input-referred)
+    ]:
+    # }}}
+    # {{{ plot labels
+    if id_string == 'pmdpx_casc12':
+        label = 'Cascade + Duplexer'
+    elif id_string == 'probe_pmdpx_casc12':
+        label = 'Cascade + Duplexer + Probe'
+    elif id_string == 'box_probe_pmdpx_casc12':
+        label = 'Cascade + Duplexer + Probe + Diode Box'
+    elif id_string == 'txbox_probe_pmdpx_casc12':
+        label = 'Cascade + Duplexer + Probe + TX Diode Box'
+    elif id_string == 'amp_box_probe_pmdpx_casc12':
+        label = 'Cascade + Duplexer + Probe + Diode Box + ENI'
+    elif id_string == 'amp_txbox_probe_pmdpx_casc12':
+        label = 'Cascade + Duplexer + Probe + TX Diode Box + ENI'
+    elif id_string == 'network_100M':
+        label = 'Network noise, 100M Samples per second'
+    elif id_string == 'network_22MHz_100M':
+        label = 'Network noise, 22 MHz filter, 100M Samples per second'
+    elif id_string == 'network_22MHz_100M_2':
+        label = 'Network noise, 22 MHz filter, elapsed; 100M Samples per second'
+    else:
+        label = date+id_string 
+    #label += ' (g=%0.2f)'%gain_factor
+   # }}}
+    print "\n*** LOADING:",id_string,"***"
+    print "FILE LABEL IS:\t\t",label
+    # {{{ calculate positive frequency noise power spectral density
+    s = load_noise(date,id_string,captures)
+    acq_time = diff(s.getaxis('t')[r_[0,-1]])[0]
+    print "ACQUISITION TIME IS:\t",acq_time
+    print "DWELL TIME IS:      \t",diff(s.getaxis('t')[r_[0,1]])[0]
+    s.ft('t',shift=True)
+    s = abs(s)**2         #mod square
+    s.mean('capture', return_error=False)
+    width = 1e6
+    #s.convolve('t',width) # we do this before chopping things up, since it uses
+    #                      fft and assumes that the signal is periodic (at this
+    #                      point, the signal at both ends is very close to
+    #                      zero, so that's good
+    s = abs(s)['t':(0,None)]
+    s /= 50.              # divide by resistance, gives units: W*s, or W/Hz
+    s /= acq_time         # divide by acquisition time
+    s *= 2               # because the power is split over negative and positive frequencies
+#    if gain_factor != 1: # if we're not talking about the scope noise
+#        s -= scope_noise
+    s /= gain_factor      # divide by gain factor, found from power curve -->
+    #                       now we have input-referred power
+    # }}}
+    if not integration:
+#        if '22MHz' in id_string: 
+#            fl.next('Low-pass Power Spectral Density (Input-referred) (convolution = %0.1e Hz)'%width)
+#            s.name('$S_{xx}(\\nu)$').set_units('W/Hz')
+#            fl.plot(s['ch',0], alpha=0.35, label="%s"%label, plottype='semilogy')
+#            axhline(y=k_B*T/1e-12, alpha=0.9, color='purple') # 1e-12 b/c the axis is given in pW
+#        elif '22MHz' not in id_string: 
+            fl.next('Power Spectral Density (Input-referred) (convolution = %0.1e Hz)'%width)
+            s.name('$S_{xx}(\\nu)$').set_units('W/Hz')
+            fl.plot(s['ch',0], alpha=0.35, label="%s"%label, plottype='semilogy')
+            axhline(y=k_B*T/1e-12, alpha=0.9, color='purple') # 1e-12 b/c the axis is given in pW
+    if integration:
+        interval = tuple(integration_center+r_[-1,1]*integration_width)
+        startf,stopf = tuple(interval)
+        print "INTEGRATION INTERVAL:",startf/1e6,"to",stopf/1e6,"MHz"
+        if 'ch' not in s.dimlabels:
+            # {{{ a hack to create a fake ch axis
+            t_label = s.getaxis('t')
+            t_units = s.get_units('t')
+            s.setaxis('t',None)
+            s.chunk('t',['t','ch'],[-1,1])
+            s.setaxis('t',t_label)
+            s.set_units('t',t_units)
+            # }}}
+#        try:
+#            s_slice = s['t':interval]['ch',0] #CH1=DUT
+#        #{{{ for scope noise test
+#        except:
+#            raise ValueError(strm("problem trying to pull the slice, shape of s is",ndshape(s),"numchan is",numchan))
+#        if gain_factor == 1.0:
+#            print "Noise coming from the scope is",s['t':interval]['ch',0].mean('t', return_error=False).data
+#
+#            #}}}
+#        else:
+        fl.next('FULL Power Spectral Density (Input-referred) (convolution = %0.1e Hz)'%width)
+        s.name('$S_{xx}(\\nu)$').set_units('W/Hz')
+#        s_slice.name('$S_{xx}(\\nu)$').set_units('W/Hz')
+        fl.plot(s['ch',0], alpha=0.35, label="%s"%label, plottype='semilogy')
+        axhline(y=k_B*T/1e-12, alpha=0.9, color='purple') # 1e-12 b/c the axis is given in pW
+#        fl.next('Power Spectral Density (Input-referred) (convolution = %0.1e Hz)'%width)
+#        fl.plot(s['t':(0,250e6)]['ch',0], alpha=0.35, label="%s"%label, plottype='semilogy')
+        #   fl.plot(s_slice, alpha=0.8, color='black', label="integration slice",
+        #      plottype='semilogy')
+        axhline(y=k_B*T/1e-12, alpha=0.9, color='purple') # 1e-12 b/c the axis is given in pW
+        #    # {{{ calculates power at input of component over specified frequency interval
+        #    if numchan == 2:
+        #        #CH1=DUT, CH2=REF(signal) or NULL(noise)
+        #        print "CH1 POWER IS:",s['t':interval]['ch',0].integrate('t')
+        #        print "CH2 POWER IS:",s['t':interval]['ch',1].integrate('t')*atten_factor*gain_factor
+        #        power_dens_CH2_dict[id_string] = (s['t':interval]['ch',1].integrate('t').data)
+        #        # }}}
+        #    power_dens_CH1_dict[id_string] = s['t':interval]['ch',0].integrate('t').data
+        #    expand_x()
+        #    print "THERMAL NOISE POWER IS:",k_B*T*float(interval[-1]-interval[0])
+        #    NF = (s['t':interval]['ch',0].integrate('t').data)/(k_B*T*float(interval[-1]-interval[0]))
+        #    print "NOISE FIGURE IS:",NF
+        #    print "EFFECTIVE TEMPERATURE IS:",(293.0*(NF-1))
+        #    print "*** EXITING:",id_string,"***"
+        #print "error is %0.12f"%(((power_dens_CH1_dict['sine_pomona_dpx_cascade12_2CH'] - power_dens_CH1_dict['noise_pomona_dpx_cascade12_2CH'] - power_dens_CH2_dict['sine_pomona_dpx_cascade12_2CH'])/power_dens_CH2_dict['sine_pomona_dpx_cascade12_2CH'])*100)
+
+fl.show()
+
