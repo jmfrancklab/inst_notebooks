3b8f651ae40e41cf03ff035f0632aed6bcf5b209
John Franck (where?) <jmfranck@syr.edu>
2018-07-05 18:57:28 -0400

    changes to hahn_echo_180704 actually included here

--- ipynb/hahn_echo_180704.py
+++ ipynb/hahn_echo_180704.py
@@ -122,6 +122,32 @@ for i,omega1 in enumerate(omega1_axis):
 results.set_units('omega1','Hz').set_units('Omega','Hz')
 results.rename('omega1',r'$\omega_1/2\pi$').rename('Omega',r'$\Omega/2\pi$')
 
+# What if we phase cycle?
+# *Note:* I changed this from 2 step to 4 step on the 180, because I thought the signal should be cleaner
+
+t90 = 2.1e-6
+omega1_nominal = 2*pi/(4*t90)
+results = ndshape([4,4,100,1000],['ph2','ph1','omega1','Omega']).alloc()
+omega1_axis = linspace(0,3*omega1_nominal,ndshape(results)['omega1'])
+omega_axis = linspace(-omega1_nominal*10,omega1_nominal*10,ndshape(results)['Omega'])
+results.labels(['ph2','ph1'],[r_[0:4],r_[0:4]])
+results.labels(['omega1','Omega'],[omega1_axis/2/pi,omega_axis/2/pi])
+tau = 50e-6
+for ph2 in xrange(0,4,2):
+    for ph1 in xrange(4):
+        for i,omega1 in enumerate(omega1_axis):
+            for j,Omega in enumerate(omega_axis):
+                pul_rot = lambda I,ph: expm(-1j*pi/2*Iz*ph).dot(I).dot(1j*pi/2*Iz*ph)
+                U90 = expm(-1j*omega1*t90*pul_rot(Ix,ph1)-1j*Omega*t90*Iz)
+                U180 = expm(-1j*omega1*2*t90*pul_rot(Ix,ph2)-1j*Omega*2*t90*Iz)
+                Uev = expm(-1j*Omega*tau*Iz)
+                Uev_echo = expm(-1j*Omega*(tau+2*t90/pi)*Iz)
+                Utot = Uev.dot(U180).dot(Uev).dot(U90)
+                rho = Utot.dot(Iz).dot(Utot.T.conj())
+                results['ph2',ph2]['ph1',ph1]['omega1',i]['Omega',j] = rho[1,0]
+results.set_units('omega1','Hz').set_units('Omega','Hz')
+results.rename('omega1',r'$\omega_1/2\pi$').rename('Omega',r'$\Omega/2\pi$')
+
 
 # 
 
@@ -129,7 +155,12 @@ fl.next('original simulation zoomed out abs',figsize=(14,14)) # I want figsize, 
 fl.image(abs(results))
 
 
-# 
+# pull the part filtered by the second pulse, to see if we can figure out -1 vs. +1
+
+fl.next('filter for second pulse',figsize=(14,14)) # I want figsize, and I need fl so that it handles the units
+figure(figsize=(14,14))
+fl.image(results['ph2',2])
+
 
 3e6/gammabar_H/1e-4
 
@@ -147,4 +178,20 @@ fl.image(abs(results))
 # 
 
 500e-4/4.258e7
+
+# Somewhat arbitrarily mixed in -- how do we actually construct the phase cycled pulses?
+
+t = r_[0:2*128]
+f = 0.25
+figure(figsize=(20,10))
+wform_noph = exp(1j*2*pi*f*t)
+wform_noph[55:150] = 0
+plot_offset = 0
+for ph_2 in xrange(0,4,2):
+    for ph_1 in xrange(4):
+        wform = wform_noph.copy()
+        wform[0:56] *= exp(1j*pi/2*ph_1)
+        wform[140:] *= exp(1j*pi/2*ph_2)
+        plot(wform.real+plot_offset)
+        plot_offset += 2
 
