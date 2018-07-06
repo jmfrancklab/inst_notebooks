7502b52611956ddc073d1c94ac091456fa73a0e1
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-07-06 10:57:12 -0400

    Update git ignore to ignore pdf files, minimal updates to chrip

--- chirp.py
+++ chirp.py
@@ -112,7 +112,7 @@ while try_again:
     data_name = 'capture%d'%j
     data.name(data_name)
     try:
-        data.hdf5_write('180628_chirp_probe_and_sample_in_magnet.h5')
+        data.hdf5_write('180702_chirp_probe_in_magnet_off_EPRon.h5')
         try_again = False
     except Exception as e:
         print e
