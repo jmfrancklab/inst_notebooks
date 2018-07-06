2ea9e7c395e448132ff83494c0dd1b0deb832f67
John Franck (where?) <jmfranck@syr.edu>
2018-07-06 11:32:40 -0400

    fixed some bugs/typos in hahn echo code

--- ipynb/hahn_echo_180704.py
+++ ipynb/hahn_echo_180704.py
@@ -65,9 +65,8 @@ fl.next('original simulation abs',figsize=(14,14)) # I want figsize, and I need 
 fl.image(abs(results))
 
 
-# 
+# try varying $B_1$ amplitude to get the 180 instead (half-amplitude 180 pulse for the 90)
 
-# try varying B1 amplitude to get the 180 instead (half-amplitude 180 pulse for the 90)
 t90 = 2.1e-6
 omega1_nominal = 2*pi/(4*t90)
 results = ndshape([100,1000],['omega1','Omega']).alloc()
@@ -100,9 +99,8 @@ fl.next('two amps simulation abs',figsize=(14,14)) # I want figsize, and I need 
 fl.image(abs(results))
 
 
-# 
-
 # how far do we have to go to not see signal at all?
+
 t90 = 2.1e-6
 omega1_nominal = 2*pi/(4*t90)
 results = ndshape([100,1000],['omega1','Omega']).alloc()
@@ -127,17 +125,18 @@ results.rename('omega1',r'$\omega_1/2\pi$').rename('Omega',r'$\Omega/2\pi$')
 
 t90 = 2.1e-6
 omega1_nominal = 2*pi/(4*t90)
-results = ndshape([4,4,100,1000],['ph2','ph1','omega1','Omega']).alloc()
+d.display(d.Markdown(r'$t_{90}$ chosen assuming an $\omega_1/2\pi=%0.2f$ kHz'%(omega1_nominal/2/pi/1e3)))
+results = ndshape([4,4,20,200],['ph2','ph1','omega1','Omega']).alloc()
 omega1_axis = linspace(0,3*omega1_nominal,ndshape(results)['omega1'])
 omega_axis = linspace(-omega1_nominal*10,omega1_nominal*10,ndshape(results)['Omega'])
 results.labels(['ph2','ph1'],[r_[0:4],r_[0:4]])
 results.labels(['omega1','Omega'],[omega1_axis/2/pi,omega_axis/2/pi])
+pul_rot = lambda I,ph: expm(1j*pi/2*Iz*ph).dot(I).dot(expm(-1j*pi/2*Iz*ph))# sense of rotation relative to propagators chosen so that signal shows up along -1
 tau = 50e-6
-for ph2 in xrange(0,4,2):
+for ph2 in xrange(4):
     for ph1 in xrange(4):
         for i,omega1 in enumerate(omega1_axis):
             for j,Omega in enumerate(omega_axis):
-                pul_rot = lambda I,ph: expm(-1j*pi/2*Iz*ph).dot(I).dot(1j*pi/2*Iz*ph)
                 U90 = expm(-1j*omega1*t90*pul_rot(Ix,ph1)-1j*Omega*t90*Iz)
                 U180 = expm(-1j*omega1*2*t90*pul_rot(Ix,ph2)-1j*Omega*2*t90*Iz)
                 Uev = expm(-1j*Omega*tau*Iz)
@@ -147,22 +146,27 @@ for ph2 in xrange(0,4,2):
                 results['ph2',ph2]['ph1',ph1]['omega1',i]['Omega',j] = rho[1,0]
 results.set_units('omega1','Hz').set_units('Omega','Hz')
 results.rename('omega1',r'$\omega_1/2\pi$').rename('Omega',r'$\Omega/2\pi$')
+results.ft(['ph1','ph2'])
 
 
 # 
 
-fl.next('original simulation zoomed out abs',figsize=(14,14)) # I want figsize, and I need fl so that it handles the units
-fl.image(abs(results))
+with figlist_var() as fl:
+    fl.next('show relative size of phcyc channels',figsize=(14,28)) # I want figsize, and I need fl so that it handles the units
+    fl.image(abs(results))
 
 
 # pull the part filtered by the second pulse, to see if we can figure out -1 vs. +1
 
-fl.next('filter for second pulse',figsize=(14,14)) # I want figsize, and I need fl so that it handles the units
-figure(figsize=(14,14))
-fl.image(results['ph2',2])
+with figlist_var() as fl:
+    fl.next('filter desired channel',figsize=(14,14)) # I want figsize, and I need fl so that it handles the units
+    figure(figsize=(14,14))
+    fl.image(results['ph2',2]['ph1',+1])
 
 
-3e6/gammabar_H/1e-4
+# 
+
+150e3/gammabar_H/1e-4
 
 
 # 
@@ -178,6 +182,7 @@ fl.image(results['ph2',2])
 # 
 
 500e-4/4.258e7
+
 
 # Somewhat arbitrarily mixed in -- how do we actually construct the phase cycled pulses?
 
