db49f72fdb275f409a619f3c8575a586b9702c76
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-07-06 13:05:45 -0400

    Significant updates to spin echo program. Can generate the waveform used as the spin echo pulse sequence via complex exponential or varying values in an array. If generating waveform via complex exponential, can then perform phase cycling. Currently captures each cycle as one node saved to an h5 file.
    This can support field sweep or static field phase cycling captures.

--- spin_echo.py
+++ spin_echo.py
@@ -8,7 +8,7 @@ import pprint
 from scipy import signal
 
 fl = figlist_var()
-
+#{{{ Initialize instruments
 print "These are the instruments available:"
 SerialInstrument(None)
 print "done printing available instruments"
@@ -18,8 +18,8 @@ with SerialInstrument('GDS-3254') as s:
     
 with SerialInstrument('AFG-2225') as s:
     print s.respond('*idn?')
-
-#{{{ calls scope and acquires signal
+    #}}}
+#{{{ acquire function, calls only scope and captures; for field sweep
 def acquire(date,id_string,captures):
 #{{{ documentation
     r'''Captures displayed waveform on scope as an hdf5 file
@@ -82,8 +82,8 @@ def acquire(date,id_string,captures):
     print "shape of data",ndshape(s)
     return
 #}}}
-#{{{ spin echo function
-def spin_echo(freq = 14.445e6, p90 = 2.27e-6, d1 = 63.794e-6, ch1_only=True):
+#{{{ spin echo function with phase cycling capability
+def spin_echo(freq = 14.445e6, p90 = 2.27e-6, d1 = 63.794e-6, T1 = 200e-3, max_delay = True, complex_exp = True, ph_cyc = True):
 #{{{ documentation
     r'''generates spin echo (90 - delay - 180) pulse sequence, defined by
     the frequency, 90 time, and delay time, and outputs on scope.
@@ -93,7 +93,7 @@ def spin_echo(freq = 14.445e6, p90 = 2.27e-6, d1 = 63.794e-6, ch1_only=True):
     ==========
     freq : float
 
-        Frequency of RF pulse
+        Frequency of RF pulse; i.e., carrier frequency
 
     p90 : float
 
@@ -101,76 +101,143 @@ def spin_echo(freq = 14.445e6, p90 = 2.27e-6, d1 = 63.794e-6, ch1_only=True):
 
     d1 : float
 
-        Delay time between 90 and 180 pulse; set to deadtime
+        Delay time between 90 and 180 pulse, akin to 'tau'; at minimum, should set to deadtime. 
+        This delay time is only applicable if max_delay = False.
 
-    ch1_only : boolean
+    T1 : float
 
-        When True, sequence is output on CH1 only.
-        When False, sequence is output on both CH1 and CH2.
+        T1 of sample, determines the burst period (i.e., the amount of time between output of pulse sequence)
+        For 13 mM [NiSO4], T1 = 200ms; with this as input, the burst period is set to 1 second.
+
+    max_delay : boolean 
+
+        Currently only set for complex_exp waveform generation.
+        If set to True, then program maximizes the delay time between the 90 pulse and the 180 pulse, which is
+        constrained by the sampling rate of the arbitrary waveform on the scope.
+        If set to False, then delay time between the 90 pulse and the 180 pulse
+        T1 of sample, determines the burst period (i.e., the amount of time between output of pulse sequence)
+        For 13 mM [NiSO4], T1 = 200ms; with this as input, the burst period is set to 1 second.
     
+    complex_exp : boolean 
+
+        If set to True, then program generates arbitrary waveform via a complex exponential. This is the only waveform that supports 
+        phase cycling. This is used as of July 2018.
+        If set to False, then program generates arbitrary waveform by setting the values of an array via 4-step slicing. This was
+        the previous waveform used for generating pulses.
+
+    ph_cyc : boolean
+
+        If set to True, then program will phase shift the 180 pulse and the 90 pulse separately, capturing each shift. The phase cycle cannot
+        be set through commands yet. Note that with this set to True, there is no need to call acquire.
+        If set to False, then program does not phase shift; to capture signal, will need to call acquire (for field sweeps). 
+
     '''
     #}}}
     start_seq = timer()
-    #{{{ generating sequence array; want to do this once 
-    rate = freq*4                                           # rate set on AFG
-    t_seq = p90 + d1 + 2*p90                                # time length of entire pulse sequence
-    pts_seq = int(t_seq*rate/4 + 0.5)*4                     # no. points needed for this time length at given rate
-    
-    t_sp = t_seq/pts_seq                                    # time between each point in sequence
-    pts_p90 = int(p90/t_sp/4 + 0.5)*4                       # no. points needed for 90 pulse (fit to integer of 4)
-    pts_d1 = int(d1/t_sp/4 + 0.5)*4                         # no. points needed for tau (90-180 delay) (int of 4)
-    pts_p180 = int(2*p90/t_sp/4 + 0.5)*4                    # no. points needed for 180 pulse (int of 4)
-
-    pts_seq = int((pts_p90 + pts_d1 + pts_p180)/4 + 0.5)*4  # no. points for entire sequence as int of 4 
-
-    assert pts_seq < 4097                                   # no. points total must be less than 4097
-    print t_sp
-    print pts_p90
-    print pts_d1
-    print pts_p180
-    print pts_seq
-    y = zeros(pts_seq)
-    y[0::4] = 0
-    y[1::4] = 1
-    y[2::4] = 0
-    y[3::4] = -1
-    y[-1] = 0
-    y[pts_p90:(pts_seq-pts_p180)] = 0
-    end_seq = timer()
+#{{{ calculating parameters for arbitrary waveform
+    d_interseq = 5*T1       #[sec] time between sequence trigger 
+    #{{{ this generates spin echo via complex exponential
+    if complex_exp:
+        freq_carrier = freq     #[Hz] rf pulse frequency
+        points_total = 4096     #[pts] total points, property of AFG
+        rate = freq_carrier*4   #[pts/sec] AFG requires for arb waveform
+        time_spacing = 1/rate   #[sec/pt] time between two points
+        time_total = time_spacing * points_total #[sec]
+        t_90 = p90              #[sec] pulse length of 90
+        t_180 = 2*t_90          #[sec] pulse length of 180
+        points_90 = t_90/time_spacing #[pts] points in 90
+        points_180 = t_180/time_spacing #[pts] points in 180
+        if not max_delay:
+            t_d1 = d1
+            points_d1 = t_d1/time_spacing
+        if max_delay:
+            points_d1 = points_total - points_90 - points_180
+            t_d1 = points_d1*time_spacing
+        time_sequence = t_90 + t_d1 + t_180
+        points_sequence = points_90 + points_d1 + points_180
+        assert points_sequence < 4097
+        #{{{ generating the arbitrary waveform
+        t = r_[0:4096]
+        freq_sampling = 0.25
+        y = 0*t
+        y = exp(1j*2*pi*t[1 : -2]*freq_sampling)
+        y[int(points_90) : int(points_90+points_d1)] = 0
+        y[int(points_90+points_d1+1)] = 0 # this starts the 180 as cosine to match 90
+        #}}}
     #}}}
+    #{{{ this generates spin echo via alternating array (previous method)
+    if not complex_exp:
+        rate = freq*4                                           # rate set on AFG
+        t_seq = p90 + d1 + 2*p90                                # time length of entire pulse sequence
+        pts_seq = int(t_seq*rate/4 + 0.5)*4                     # no. points needed for this time length at given rate
+        t_sp = t_seq/pts_seq                                    # time between each point in sequence
+        pts_p90 = int(p90/t_sp/4 + 0.5)*4                       # no. points needed for 90 pulse (fit to integer of 4)
+        pts_d1 = int(d1/t_sp/4 + 0.5)*4                         # no. points needed for tau (90-180 delay) (int of 4)
+        pts_p180 = int(2*p90/t_sp/4 + 0.5)*4                    # no. points needed for 180 pulse (int of 4)
+        pts_seq = int((pts_p90 + pts_d1 + pts_p180)/4 + 0.5)*4  # no. points for entire sequence as int of 4 
+        assert pts_seq < 4097                                   # no. points total must be less than 4097
+        #{{{ generating the arbitrary waveform
+        y = zeros(pts_seq)
+        y[0::4] = 0
+        y[1::4] = 1
+        y[2::4] = 0
+        y[3::4] = -1
+        y[-1] = 0
+        y[pts_p90:(pts_seq-pts_p180)] = 0
+        #}}}
+    #}}}
+#}}}
+    end_seq = timer()
     with AFG() as a:
         a.reset()
-        #{{{ output of array, takes 4 seconds
         ch_list = [0]
         for this_ch in ch_list:
             a[this_ch].ampl = 10.
             a[this_ch].digital_ndarray(y, rate=rate)
-            end_loadseq = timer()
-            start_out = timer()
             a[this_ch].output = True
             a[this_ch].burst = True
-            end_out = timer()
-            d_interseq = 5*200e-3
-            a.set_burst(per=d_interseq) #per=T2 of sample
-            #}}}
-    return start_seq,end_seq,end_loadseq,start_out,end_out
+            a.set_burst(per=d_interseq)
+            #{{{
+            if ph_cyc:
+                y_ph = y.copy()
+                for ph2 in xrange(0,4,2):
+                    y_ph[int(points_90+points_d1+1):-2] *= exp(1j*pi/2*ph2)
+                    for ph1 in xrange(4):
+                        y_ph[1:int(points_90)] *= exp(1j*pi/2*ph1)
+                        a[this_ch].digital_ndarray(y_ph, rate=rate)
+                        a[this_ch].burst = True
+                        with GDS_scope() as g:
+                            ch1_wf = g.waveform(ch=1)
+                            ch2_wf = g.waveform(ch=2)
+                            #{{{ this could be used to do several rounds of phase cycling
+                       #     if (ph1 == 0 + ph2 == 0):
+                       #         channels = ((ndshape(data)) + ('capture',cap_len)).alloc()
+                       #         channels.setaxis('t',data.getaxis('t')).set_units('t','s')
+                       #         channels.setaxis('ch',data.getaxis('ch'))
+                       # channels['capture',x-1] = data
+                        #}}}
+                        data = concat([ch1_wf,ch2_wf],'ch').reorder('t')
+                        data_name = str(ph1)+str(ph2)
+                        data.name(data_name)
+                        data.hdf5_write(date+"_"+id_string+".h5")
+                        print "name of data",data.name()
+                        print "units should be",data.get_units('t')
+                        print "shape of data",ndshape(data)
+                #}}}
+    return start_seq,end_seq
 #}}}
 
-date = '180630'
-id_string = 'spin_echo_exp_block1'
-captures = linspace(1,6000,6000)
+date = '180706'
 
-#{{{ these are used in the collect_until program
-sweep_time = 3072 #[s] - get this from xepr parameters for current sweep
-capture_time = 1518 #[s] - approx, from timing captures for desired capture length 
-#}}}
-t1,t2,t3,t4,t5 = spin_echo()
-raw_input("Start magnetic field sweep")
-start_acq = timer()
-acquire(date,id_string,captures)
-end_acq = timer()
+id_string = 'spin_echo_phcyc_test_again'
+
+t1,t2 = spin_echo()
+#raw_input("Start magnetic field sweep")
+#start_acq = timer()
+#acquire(date,id_string,captures)
+#end_acq = timer()
 
 print "Time to generate sequence array:",t2-t1,"s"
-print "Time to generate + load sequence into AFG:",t3-t1,"s"
-print "Time to output sequence:",t5-t4,"s"
-print "Time for captures:",end_acq - start_acq,"s"
+#print "Time to generate + load sequence into AFG:",t3-t1,"s"
+#print "Time to output sequence:",t5-t4,"s"
+#print "Time for captures:",end_acq - start_acq,"s"
