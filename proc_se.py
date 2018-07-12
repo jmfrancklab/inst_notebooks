12e1ff445c576ce8b2f754112678893be5e449ea
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-07-12 17:09:56 -0400

    Transferred processing code from 'fix_phase_cycling_180712' worked on with JF to this current version of spin echo processing file; did not bother to save older work since it has been rehashed and greatly simplified in this current version (and version history can always be reviewed if needed)

--- proc_se.py
+++ proc_se.py
@@ -5,213 +5,133 @@ import sys
 import matplotlib.style
 import matplotlib as mpl
 
-mpl.rcParams['image.cmap'] = 'jet'
+#mpl.rcParams['image.cmap'] = 'jet'
 fl = figlist_var()
-
-shift_time = True
 
 for date,id_string,numchan in [
         #('180708','spin_echo',2),
-        ('180710','SE_test_phcyc_2',2),
+        #('180710','SE_test_phcyc_2',2),
         #('180711','SE_phcyc_control',2),
         #('180711','SE_phcyc_test',2),
+        ('180711','SE_phcyc_control',2)
         ]:
     filename = date+'_'+id_string+'.h5'
     nodename = 'this_capture'
 
     s = nddata_hdf5(filename+'/'+nodename,
             directory = getDATADIR(exp_type='test_equip'))
-    #s.reorder('ch','ph1','ph2','average','t')
-    if shift_time:
-        s = s.mean('average',return_error=False)
-        s1 = s['ch',1] # pull ref ch
-        s1.set_units('t','s')
-        s1.name('Amplitude $/$ $V$')
-                #{{{ will plot time domain signal for each phase, before manipulation
-        for ph2 in xrange(0,2):
-            for ph1 in xrange(4):
-                print ph1,ph2,ndshape(s1['ph1',ph1]['ph2',ph2])
-                #fl.next('Phase Cycled Spin Echo, Before Time Shift')
-                #fl.plot(s1['ph1',ph1]['ph2',ph2],label='ph1 %d, ph2 %d'%(ph1,ph2),alpha=0.25)
-                #}}}
-        # Calculating analytic signal
-        s1.ft('t',shift=True)
-        s1 = s1['t':(0,None)]
-        s1.ift('t')
-        s1 = abs(s1)
-        # Slice for the 90 pulse
-        s1 = s1['t':(0,15e-6)]
-        print ndshape(s1)
-        #{{{ block for calculating time shift
-        pulse_slice_list = []
-        time_slice_list = []
-        for ph2 in xrange(0,2):
-            for ph1 in xrange(4):
-                #{{{ use to check threshold is appropriate for pulse slice
-                #fl.next('Checking threshold')
-                #fl.plot(s1['ph1',ph1]['ph2',ph2])
-                #}}}
-                pulse_slice_list = (s1['ph1',ph1]['ph2',ph2]).contiguous(lambda x: x > 0.05*x.data.max())
-                print "Pulse slices for time shifting"
-                print pulse_slice_list
-                # calculating average time for each 90 pulse
-        for x in xrange(len(pulse_slice_list)):
-            temp = tuple(pulse_slice_list[x])
-            time_diff = temp[1] - temp[0]
-            time_slice_list.append(time_diff)
-        time_avg = sum(time_slice_list)/len(time_slice_list)
-        print "**** time average to be applied for time shifting ***"
-        print time_avg
-            #}}}
-        s1_shift = s['ch',1].C
-        s1_shift.ft('t',shift=True)
-        # performing the time shift (in the frequency domain)
-        s1_shift *= s1_shift.fromaxis('t',lambda f: exp(-1j*2*pi*f*time_avg))
-        s1_shift.ift('t')
-        s1_shift.set_units('t','s')
-        s1_shift.name('Amplitude $/$ $V$')
-                #{{{ will plot time domain signal for each phase, time shifted
-        for ph2 in xrange(0,2):
-            for ph1 in xrange(4):
-                print ph1,ph2,ndshape(s1_shift['ph1',ph1]['ph2',ph2])
-                #fl.next('Phase Cycled Spin Echo, After Time Shift')
-                #fl.plot(s1_shift['ph1',ph1]['ph2',ph2],label='ph1 %d, ph2 %d'%(ph1,ph2),alpha=0.25)
-                #}}}
-        s1_shift.ft('t')
-        # filtering
-        s1_shift = s1_shift['t':(0,15.5e6)]
-        # mix down by carrier f 
-        s1_shift.setaxis('t',lambda x: x - 14.43e6)
-        s1_shift.ift('t')
-        fl.next('ph1 ph2')
-        fl.image(s1_shift)
-        s1_noshift = s['ch',1].C
-        s1_noshift.set_units('t','s')
-        s1_noshift.name('Amplitude $/$ $V$')
-        s1_noshift.ft('t')
-        # filtering
-        s1_noshift = s1_noshift['t':(0,15.5e6)]
-        # mix down by carrier f 
-        s1_noshift.setaxis('t',lambda x: x - 14.43e6)
-        s1_noshift.ift('t')
-        fl.next('ph1 ph2 no shift')
-        fl.image(s1_noshift)
-        s1_shift.ft(['ph1','ph2'])
-        fl.next('ft ph1,ph2')
-        fl.image(s1_shift)
-        s1_noshift.ft(['ph1','ph2'])
-        fl.next('ft ph1,ph2 no shift')
-        fl.image(s1_noshift)
-        fl.show()
-        quit()
-            #{{{ block for calculating zero-order correction
-        shiftedp_slice_list = []
-        shiftedt_slice_list = []
-        for ph2 in xrange(0,2):
-            for ph1 in xrange(4):
-                #{{{ see comment below
-                    # initially used this to re-calculate pulse length, but that is not needed
-                    # since it should be the same as previous time average; however, this looks
-                    # really weird
-                #s1_shift.ft('t')
-                #s1_shift = s1_shift['t':(0,None)]
-                #s1_shift.ift('t')
-                #s1_shift = abs(s1_shift)
-                #s1_shift = s1_shift['t':(0,15e-6)]
-                #fl.next('Checking threshold time shifted pulse')
-                #fl.plot(s1_shift['ph1',ph1]['ph2',ph2])
-                #}}}
-                shiftedp_slice_list = (s1_shift['ph1',ph1]['ph2',ph2]).contiguous(lambda x: x > 0.5*x.data.max())
-                print "Pulse slices for zero order phase correction"
-                print shiftedp_slice_list
-                # calculating average time for each time-shifted 90 pulse
-        for x in xrange(len(shiftedp_slice_list)):
-            temp = tuple(shiftedp_slice_list[x])
-            time_diff = temp[1] - temp[0]
-            shiftedt_slice_list.append(time_diff)
-        time_avg = sum(shiftedt_slice_list)/len(shiftedt_slice_list)
-        print "**** time average to be applied for zero order phase correction ****"
-        print time_avg
-            #}}}
+    print "*** Code taken from 'fix_phase_cycling_180712.py' ***"
+    s.set_units('t','s')
+    s_raw = s.C.reorder('t',first=False)
 
+    fl.next('raw data')
+    fl.plot(s_raw['ch',1]['average',0]['ph2',0]['t':(24e-6,30e-6)].reorder('t').real)
 
-    if not shift_time:
-        s.reorder('ph1','ph2','average','t')
-        s.set_units('t','s')
-        print ndshape(s)
-        s.mean('average',return_error=False) 
-        # {{{ for 180709_spin_echo.h5 - only the last average slice has data, pull it
-        #fl.next('raw data')
-        #fl.image(s['average',0:-1])
-        ##s = s['average',-1]
-        # }}}
-        print ndshape(s)
-        #s.reorder('ph1','ph2','ch','t')
-        s_shift = s.C
-        s = abs(s)
-        s.ft('t',shift=True)
-        s = s['t':(0,None)]
-        s.ift('t')
-        for ph2 in xrange(0,2):
-            for ph1 in xrange(4):
-                print ph1,ph2,ndshape(s['ph1',ph1]['ph2',ph2])
-                fl.next('analytic, unfiltered')
-                fl.plot(s['ph1',ph1]['ph2',ph2],label='ph1 %d, ph2 %d'%(ph1,ph2),alpha=0.25)
-        print "done printing"
-        for ph1 in xrange(0,6,3):
-            s_shift['ph1',ph1].setaxis('t',lambda x: x+0)
-        s_shift = abs(s_shift)
-        s_shift.ft('t',shift=True)
-        s_shift = s_shift['t':(0,None)]
-        s_shift.ift('t')
-        for ph2 in xrange(0,2):
-            for ph1 in xrange(4):
-                print ph1,ph2,ndshape(s_shift['ph1',ph1]['ph2',ph2])
-                fl.next('analytic, unfiltered, shifted')
-                fl.plot(s_shift['ph1',ph1]['ph2',ph2],label='ph1 %d, ph2 %d'%(ph1,ph2),alpha=0.25)
-        print 'this has worked'
-        fl.show()
-        quit()
-        #for ph2 in xrange(0,2):
-        #    for ph1 in xrange(4):
-        #        fl.next('analytic, unfiltered freq domain')
-        #        fl.plot(s['ph1',ph1]['ph2',ph2],alpha=0.1,linestyle=':')
-        #s.ift('t')
-        for ph2 in xrange(0,2):
-            for ph1 in xrange(4):
-                fl.next('analytic, unfiltered time domain')
-                fl.plot(s['ph1',ph1]['ph2',ph2],alpha=0.1,linestyle=':')
-                print "trying to plot"
-                fl.show()
-        quit()
-        # band pass filtering
-        #s['t':(None,10e6)] = 0
-        #s['t':(20e6,None)] = 0
-        s = s['t':(0,15.5e6)]
-        # mixing down
-            #{{{ the 0.33/4e-6 comes from looking at the low pass filtered,
-            #       phase FT'd image, and seeing how the color following the pulses
-            #       changes, which relays the coherence level difference via color plot (see
-            #       doi:10.1063/1.3243850 for more details) -- for '180708_spin_echo.h5',
-            #       the color changes from blue --> green, which indicates a shift of 2pi/3
-            #       or 0.33; this is the phase shift for the 180 pulses. Best seen on ref ch.
-            #}}}
-        #s.setaxis('t',lambda x: x-14.5e6+0.33/4e-6)
-        #s.setaxis('t',lambda x: x-14.5e6+0.33/4e-6)
-        #s.setaxis('t',lambda x: x-14.4247e6-0.02/((2*2.63)*1e-6))
-        s.setaxis('t',lambda x: x-14.4247e6-0.25/time_avg)
-        print ndshape(s)
-        s.ift('t')
-        #s.ft(['ph1','ph2'])
-        fl.next('filtered')
-        fl.image(s['ch',1])
-        #fl.next('filtered, cropped')
-        #fl.image(s['ch',0].cropped_log())
-        fl.show()
-        quit()
-        #fl.plot(s['ph1',0]['ph2',0])
-        #fl.plot(s['ph1',1]['ph2',0])
-        #fl.plot(s['ph1',2]['ph2',0])
-        #fl.plot(s['ph1',3]['ph2',0])
-        #fl.plot(s['ph1',0]['ph2',1])
+    print ndshape(s)
+    print ndshape(s_raw)
 
+    s.ft('t',shift=True)
+    s = s['t':(0,None)]
+    s.setaxis('t',lambda f: f-14.4247e6)
+    s.ift('t')
+
+    fl.next('phcyc')
+    # subset of interest in the data, undergoes processing to analytic signal
+    subset = s['ch',1]['t':(20e-6,100e-6)]
+    fl.image(subset,black=True)
+    onephase = subset.C.smoosh(['ph2','average'], noaxis = True, dimname='repeat').reorder('t')
+    print "dimensions of data subset of interest",ndshape(onephase)
+    # perform same analysis used on subset for raw data, to compare
+    onephase_raw = s_raw['ch',1].C.smoosh(['ph2','average'], noaxis=True, dimname='repeat').reorder('t')
+    print "dimensions of re-grouped raw data",ndshape(onephase_raw)
+    colors = ['r','g','b','c']
+    for k in xrange(ndshape(onephase)['ph1']):
+        for j in xrange(ndshape(onephase)['repeat']):
+            fl.next('compare rising edge')
+            # need to define time slice for rising edge
+            fl.plot(abs(onephase['repeat',j]['t':(24.85e-6,25e-6)]['ph1',k].C.reorder('t',first=True)),color=colors[k],alpha=0.3)
+            fl.next('compare falling edge')
+            # need to define time slice for falling edge
+            fl.plot(abs(onephase['repeat',j]['t':(27.4e-6,27.8e-6)]['ph1',k].C.reorder('t',first=True)),color=colors[k],alpha=0.3)
+            fl.next('compare rising edge, raw')
+            fl.plot((onephase_raw['repeat',j]['t':(24.85e-6,25e-6)]['ph1',k].C.reorder('t',first=True)),color=colors[k],alpha=0.3)
+            fl.next('compare falling edge, raw')
+            fl.plot((onephase_raw['repeat',j]['t':(27.4e-6,27.8e-6)]['ph1',k].C.reorder('t',first=True)),color=colors[k],alpha=0.3)
+    print ndshape(onephase)
+
+    # the above should confirm that pulses are triggered differently due to their different rising edges
+
+    #{{{ defining new, more convenient x-axis below
+    # note, pulse length used below is manually determined
+    pulse_slice = s_raw['t':(24.8e-6,27.6e-6)]['ch',1].real
+    normalization = (pulse_slice**2).integrate('t')
+    # this creates an nddata of the time averages for each 90 pulse
+    average_time = (pulse_slice**2 * pulse_slice.fromaxis('t')).integrate('t')/normalization
+    average_time.reorder('average',first=False)
+    # shift the time axis down by the average time, so that 90 is centered around t=0
+    s_raw.setaxis('t', lambda t: t-average_time.data.mean())
+    # check that this centers 90 around 0 on time axis
+    print "*** Check that time-shifted data plots 90 pulse around 0 ***"
+    fl.next('time-shifted data')
+    fl.image(s_raw)
+    #}}}
+    pulse_slice = s_raw['t':(-1.6e-6,1.6e-6)]['ch',1].real
+    # re-determine nddata of the time averages for the re-centered data
+    average_time = (pulse_slice**2 * pulse_slice.fromaxis('t')).integrate('t')/normalization
+    average_time.reorder('average',first=False)
+    
+    analytic = s_raw.C.ft('t',shift=True)['t':(0,None)]
+    analytic.setaxis('t',lambda f: f-14.4247e6)
+    phase_factor = analytic.fromaxis('t',lambda x: 1j*2*pi*x)
+    phase_factor *= average_time
+    phase_factor.run(lambda x: exp(x))
+    analytic *= phase_factor
+    analytic.ift('t')
+
+    fl.next('analytic signal, phased, time domain')
+    fl.image(analytic)
+
+    # beginning phase correction now
+    # time-shift the raw data (done above), then apply phase correction
+    onephase_raw_shift = s_raw['ch',1].C.smoosh(['ph2','average'],noaxis=True,dimname='repeat').reorder('t')
+    for k in xrange(ndshape(onephase_raw)['ph1']):
+        for j in xrange(ndshape(onephase_raw)['repeat']):
+            fl.next('compare rising edge: uncorrected')
+            fl.plot((onephase_raw_shift['repeat',j]['ph1',k]['t':(-1.5e-6,-1.1e-6)].C.reorder('t',first=True)+2*k),color=colors[k],alpha=0.3)
+            fl.next('compare falling edge: uncorrected')
+            fl.plot((onephase_raw_shift['repeat',j]['ph1',k]['t':(1.1e-6,1.5e-6)].C.reorder('t',first=True)+2*k),color=colors[k],alpha=0.3)
+    raw_corr = s_raw.C.ft('t',shift=True)
+    # sign on 1j matters here, difference between proper cycling or off cycling
+    phase_factor = raw_corr.fromaxis('t',lambda x: 1j*2*pi*x)
+    phase_factor *= average_time
+    phase_factor.run(lambda x: exp(x))
+    raw_corr *= phase_factor
+    # here zero filling or else signal amplitude will vary, due to changes made in the f dimension 
+    raw_corr.ift('t',pad=30*1024)
+    onephase_rawc = raw_corr['ch',1].C.smoosh(['ph2','average'],noaxis=True, dimname='repeat').reorder('t')
+    print "*** shape of raw, corrected re-grouped data ***"
+    print ndshape(onephase_rawc)
+    for k in xrange(ndshape(onephase_rawc)['ph1']):
+        for j in xrange(ndshape(onephase_rawc)['repeat']):
+            fl.next('compare rising edge: corrected')
+            fl.plot(onephase_rawc['repeat',j]['ph1',k]['t':(-1.5e-6,-1.1e-6)].C.reorder('t',first=True),color=colors[k],alpha=0.3)
+            fl.next('compare falling edge: corrected')
+            fl.plot(onephase_rawc['repeat',j]['ph1',k]['t':(1.1e-6,1.5e-6)].C.reorder('t',first=True),color=colors[k],alpha=0.3)
+    
+    # with time-shifted, phase corrected raw data, now take analytic
+    analytic = raw_corr['ch',1].C.ft('t')['t':(0,15.5e6)].setaxis('t', lambda f: f-14.4247e6).ift('t').reorder(['average','t'],first=False)
+    # measured phase is the result obtained from data after time-shifting and phase correcting
+    measured_phase = analytic['t':(-2e6,2e6)].mean('t',return_error=False).mean('ph2',return_error=True).mean('average',return_error=True)
+    measured_phase /= abs(measured_phase)
+    # expected phase is how we expect the phases to cycle, and how it is programmed in the pulse sequence
+    expected_phase = nddata(exp(r_[0,1,2,3]*pi/2*1j),[4],['ph1'])
+    # phase correcting analytic signal by difference between expected and measured phases
+    analytic *= expected_phase/measured_phase
+    fl.next('analytic signal')
+    fl.image(analytic['t':(-2e-6,75e-6)])
+    # switch to coherence domain
+    fl.next('coherence domain')
+    coherence_domain = analytic.C.ift(['ph1','ph2'])
+    fl.image(coherence_domain['t':(-2e-6,75e-6)])
+fl.show()
+quit()
