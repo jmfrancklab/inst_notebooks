f7e82aa6ad0835fd080f3c9122c31fcc620c6dbb
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-07-14 18:15:18 -0400

    Before beginning spin echo measurements, captured and processed noise measurements to check noise level of spectrometer and captured and processed chirp measurements to check tuning of probe

--- collect100.py
+++ collect100.py
@@ -51,9 +51,9 @@ def collect(date,id_string,captures):
     print "shape of data",ndshape(s)
     return start
 
-date = '180713'
+date = '180714'
 #id_string = 'control_pulse_22MHz_2p5GSPS_zoom'
-id_string = 'test_noise_50mVd_nomag'
+id_string = 'test_noise_20mVd_3'
 #id_string = 'network_9_4'
 captures = linspace(1,100,100)
 
