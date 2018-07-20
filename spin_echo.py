cdd7a3e4430579982c670631f581eba9762f76fa
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-07-20 13:58:20 -0400

    Incorporated the nutation code into exisiting spin_echo program, where nutation() is just a function that can be called separate from spin_echo()

--- spin_echo.py
+++ spin_echo.py
@@ -19,69 +19,6 @@ with SerialInstrument('GDS-3254') as s:
 with SerialInstrument('AFG-2225') as s:
     print s.respond('*idn?')
     #}}}
-#{{{ acquire function, calls only scope and captures; for field sweep
-def acquire(date,id_string,captures):
-#{{{ documentation
-    r'''Captures displayed waveform on scope as an hdf5 file
-    saved with filename 'date_id_string'; data is stored in
-    an accumulated node, different from earlier versions.
-    Parameters
-    ==========
-    date : string 
-
-        Date of data collection
-
-    id_string : string 
-
-        Name associated with data collection
-
-    captures : numpy.ndarray
-
-        An array, beginning with 1, that specifies number of times
-        scope should capture signal - e.g., captures = linspace(1,100,100) 
-        will capture signal 100 times, beginning with capture1 and ending
-        with capture100
-    
-    '''
-    #}}}
-    cap_len = len(captures)
-    datalist = []
-    #cap_time = zeros(cap_len)
-    print "about to load GDS"
-    with GDS_scope() as g:
-    #    g.timscal(5e-6)  #setting time scale to 500 ns/div
-    #    g.voltscal(1,500e-3) #setting volt scale on channel 1 to 500 mV/div
-        print "loaded GDS"
-        for x in xrange(1,cap_len+1):
-            print "entering capture",x
-            #cap_time[x-1] = timer() - start_acq
-            ch1_waveform = g.waveform(ch=1)
-            ch2_waveform = g.waveform(ch=2)
-            data = concat([ch1_waveform,ch2_waveform],'ch').reorder('t')
-            if x == 1:
-                channels = ((ndshape(data)) + ('capture',cap_len)).alloc()
-                channels.setaxis('t',data.getaxis('t')).set_units('t','s')
-                channels.setaxis('ch',data.getaxis('ch'))
-#              channels.setaxis('cap_time',cap_time).set_units('t','s')
-            channels['capture',x-1] = data
-    # {{{ in case it pulled from an inactive channel
-    if not isfinite(data.getaxis('t')[0]):
-        j = 0
-        while not isfinite(data.getaxis('t')[0]):
-            data.setaxis('t',datalist[j].getaxis('t'))
-            j+=1
-            if j == len(datalist):
-                raise ValueError("None of the time axes returned by the scope are finite, which probably means no traces are active??")
-    # }}}
-    s = channels
-    s.labels('capture',captures)
-    s.name('accumulated_'+date)
-    s.hdf5_write(date+'_'+id_string+'.h5')
-    print "name of data",s.name()
-    print "units should be",s.get_units('t')
-    print "shape of data",ndshape(s)
-    return
-#}}}
 #{{{ spin echo function with phase cycling capability
 def spin_echo(num_cycles, freq = 14.4289e6, p90 = 2.551e-6, d1 = 63.794e-6, T1 = 200e-3, max_delay = True, complex_exp = True, ph_cyc = True):
 #{{{ documentation
@@ -200,10 +137,10 @@ def spin_echo(num_cycles, freq = 14.4289e6, p90 = 2.551e-6, d1 = 63.794e-6, T1 =
             if ph_cyc:
                 num_ph1_steps = 4 
                 num_ph2_steps = 2
-                raw_input('Begin field sweep')
                 start_ph = time.time()
                 timer_index = 0
                 for x in xrange(num_cycles):
+                    raw_input('Update field, then continue')
                     for ph2 in xrange(0,4,num_ph2_steps):
                         for ph1 in xrange(num_ph1_steps):
                             y_ph = y.copy()
@@ -266,9 +203,133 @@ def spin_echo(num_cycles, freq = 14.4289e6, p90 = 2.551e-6, d1 = 63.794e-6, T1 =
     end_ph = time.time() 
     return start_ph,end_ph 
 #}}}
+#{{{ nutation curve with spin echo
+def nutation(freq = 14.4289e6, min_t_90, max_t_90, step, T1 = 200e-3, ph_cyc = True):
+#{{{ documentation
+    r'''essentially a modification to the spin_echo(), but with enough changes to warrant
+    separate function for now. Sweeps through a range of 90 times specified by the user and
+    generates a spin echo for each using the same interpulse delay. Phase cycles through
+    each pulse sequence. Saves data to one node in an h5 file as a multidimensional nddata.
+    
+    Parameters
+    ==========
+    freq : float
 
-date = '180719'
-id_string = 'SE_sweep_3'
-num_cycles = 10 
-t1,t2 = spin_echo(num_cycles = num_cycles)
+        Frequency of RF pulse; i.e., carrier frequency
+
+    min_t_90 : float
+
+        Minimum 90 time.
+
+    max_t_90 : float
+
+        Maximum 90 time. Interpulse delay is calculated based on this value.
+
+    step : int
+
+        Spacing between minimum and maximum 90 times.
+
+    T1 : float
+
+        T1 of sample, determines the burst period (i.e., the amount of time between output of pulse sequence)
+        For 13 mM [NiSO4], T1 = 200ms; with this as input, the burst period is set to 1 second.
+
+    ph_cyc : boolean
+
+        If set to True, then program will phase shift the 180 pulse and the 90 pulse separately, capturing each shift. The phase cycle cannot
+        be set through commands yet. Currently must be set to True. 
+
+    '''
+    #}}}
+    points_total = 4096     #[pts] total points, property of AFG
+    freq_carrier = freq     #[Hz] rf pulse frequency
+    rate = freq_carrier*4   #[pts/sec] AFG requires for arb waveform
+    freq_sampling = 0.25    #effectively the 'step' of the time axis
+    time_spacing = 1/rate   #[sec/pt] time between two points
+
+    #{{{ calculate the maximum allowable delay, given that it must be held constant
+        # for all sequences generating the nutation curve
+    points_max_90 = max_t_90/time_spacing
+    points_max_180 = 2*max_t_90/time_spacing
+    points_max_d1 = points_total - points_max_90 - points_max_180
+    t_d1 = points_max_d1*time_spacing
+    #}}}
+    t_90_range = linspace(min_t_90,max_t_90,step)
+
+    # ready to start 
+    start_prog = time.time()
+    for x,t_90 in enumerate(t_90_range):
+        t_180 = 2*t_90
+        t_total = t_90 + t_d1 + t_180
+
+        points_90 = t_90/time_spacing
+        points_d1 = t_d1/time_spacing
+        points_180 = t_180/time_spacing
+        points_seq = points_90 + points_d1 + points_180
+        assert (points_seq < 4097), "Too many points"
+        # generate waveform
+        t = r_[0:int(points_seq)]
+        y = exp(1j*2*pi*t[1 : -1]*freq_sampling)
+        y[int(points_90) : int(points_90+points_d1)] = 0
+        y[0] = 0
+        y[-1] = 0
+
+        with AFG() as a:
+            a.reset()
+            ch_list = [0]
+            for this_ch in ch_list:
+                a[this_ch].ampl = 20.e-3
+                a[this_ch].digital_ndarray(y, rate=rate)
+                a[this_ch].output = True
+                a[this_ch].burst = True
+                a.set_burst(per=d_interseq)
+                a[this_ch].ampl = 10.
+                if ph_cyc:
+                    num_ph1_steps = 4
+                    num_ph2_steps = 2
+                    for ph2 in xrange(0,4,num_ph2_steps):
+                        for ph1 in xrange(num_ph1_steps):
+                            y_ph = y.copy()
+                            y_ph[1:int(points_90)] *= exp(1j*ph1*pi/2)
+                            y_ph[int(points_90+points_d1):-1] *= exp(1j*ph2*pi/2)
+                            a[this_ch].ampl = 20e-3
+                            a[this_ch].burst = True
+                            a[thic_ch].ampl = 10
+                            with GDS_scope() as g:
+                                g.acquire_mode('average',2)
+                                time.sleep(4*d_interseq) # 4 seconds to average
+                                print "Acquiring..."
+                                ch1_wf = g.waveform(ch=1)
+                                ch2_wf = g.waveform(ch=2)
+                                time_acq = time.time()
+                                g.acquire_mode('sample')
+                            if (ph1 == 0 and ph2 == 0 and x == 0):
+                                t_axis = ch1_wf.getaxis('t')
+                                data = ndshape([step,num_ph1_steps,num_ph2_steps,len(t_axis),2],
+                                        ['t90_values','ph1','ph2','t','ch']).alloc(dtype=float64)
+                                data.setaxis('t',t_axis).set_units('t','s')
+                                data.setaxis('ch',r_[1,2])
+                                data.setaxis('ph1',r_[0:4])
+                                data.setaxis('ph2',r_[0,2])
+                                data.setaxis('t90_values',t_90_range)
+                            data['t90_values',t_90]['ph1':ph1]['ph2':ph2]['ch',0] = ch1_wf
+                            data['t90_values',t_90]['ph1':ph1]['ph2':ph2]['ch',1] = ch1_wf
+                            print "**********"
+                            print "90 TIME ",t_90,",INDEX NO.",x
+                            print "ph1",ph1
+                            print "ph2",ph2
+                            print "Done acquiring" 
+    end_prog = time.time()
+    data.name("this_capture")
+    data.hdf5_write(date+"_"+id_string+".h5")
+    return start_prog,end_prog 
+#}}}
+date = '180720'
+id_string = 'SE_nutation'
+num_cycles = 13 
+max_90 = 1.5e-6
+min_90 = 6.0e-6
+step = 5
+#t1,t2 = spin_echo(num_cycles = num_cycles)
+t1,t2 = nutation(min_90,max_90,step)
 print "Time:",t2-t1,"s"
