7b5805c5e5d5e274f8cc285ffe5572c38ffa0eef
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-09-13 22:36:04 -0400

    Results for testing unshielded probe, using chip capacitors and newer variable capacitors

--- chirp.py
+++ chirp.py
@@ -150,7 +150,7 @@ while try_again:
     data_name = 'capture%d'%j
     data.name(data_name)
     try:
-        data.hdf5_write('180912_test_L_1.h5')
+        data.hdf5_write('180913_test_probe_8.h5')
         try_again = False
     except Exception as e:
         print e
