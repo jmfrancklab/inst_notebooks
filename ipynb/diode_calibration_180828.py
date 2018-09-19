69d2e8412699192d160356813a02a77a4704664c
John Franck (laptop) <jmfranck@syr.edu>
2018-09-18 21:26:01 -0400

    plots that I sent to thorsten

--- ipynb/diode_calibration_180828.py
+++ ipynb/diode_calibration_180828.py
@@ -23,9 +23,14 @@ d
 
 # 
 
+print d.data.min()
+
+
 # ## since I want to use inverse interpolation, check that it looks OK
 
-plot(d)
+# 
+
+plot(d,'o')
 yvals = linspace(3,390,100)
 xvals = d.C.invinterp('dBm',yvals)
 plot(xvals)
@@ -33,11 +38,24 @@ plot(xvals)
 
 # 
 
-readings = r_[13.4,277.9,320.9]
+readings = r_[13.4,13.8,277.9,320.9,11.6,99.4,40.2,24.5,16.2,11.4,6.5,4.3]
 for reading in readings:
     dBm_val = d.C.invinterp('dBm',reading).getaxis('dBm').item().real
-    display(md('A reading of %f mV corresponds to %f dBm'%(reading,dBm_val)))
+    display(md('A reading of %f mV corresponds to %f dBm, if dir. coup %f dBm total, if before amp appropriate for %f dBm total'%(reading,dBm_val,dBm_val+20,dBm_val+35)))
 
 
-
+# ## show how the source readings are off
+set_read_pairs = [(35,40.2),
+        (32,24.5),
+        (30,16.2),
+        (28,11.4),
+        (25,6.5),
+        (22.5,4.3),
+        ]
+settings,readings = zip(*set_read_pairs)
+dBm_val = d.C.invinterp('dBm',readings).getaxis('dBm').item().real
+dBm_val += 35. # what setting is the appropriate for
+plot(settings,dBm_val,'o')
+xlabel('setting / dBm')
+ylabel('(source ouput + 35 dB) / dBm')
 
