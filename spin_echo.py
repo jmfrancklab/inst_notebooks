a44c21a7a2777ecca770a1eabde4e37da36199d2
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-11-03 16:01:52 -0400

    Fixed error in arbitrary wf generation for spin echo

--- spin_echo.py
+++ spin_echo.py
@@ -22,7 +22,7 @@ with SerialInstrument('AFG-2225') as s:
     print s.respond('*idn?')
     #}}}
 #{{{ spin echo function with phase cycling capability
-def spin_echo(num_cycles, freq=14.46e6, p90=0.9e-6, d1=63.794e-6, T1=200e-3, max_delay=True, complex_exp=True, ph_cyc=True, field_sweep=False):
+def spin_echo(num_cycles, freq=14.46e6, p90=0.9e-6, d1=27.0e-6, T1=200e-3, max_delay=False, complex_exp=True, ph_cyc=True, field_sweep=False):
 #{{{ documentation
     r'''generates spin echo (90 - delay - 180) pulse sequence, defined by
     the frequency, 90 time, and delay time, and outputs on scope.
@@ -88,6 +88,7 @@ def spin_echo(num_cycles, freq=14.46e6, p90=0.9e-6, d1=63.794e-6, T1=200e-3, max
         if not max_delay:
             t_d1 = d1
             points_d1 = t_d1/time_spacing
+            points_total = points_90 + points_d1 + points_180
         if max_delay:
             points_d1 = points_total - points_90 - points_180
             t_d1 = points_d1*time_spacing
@@ -95,7 +96,7 @@ def spin_echo(num_cycles, freq=14.46e6, p90=0.9e-6, d1=63.794e-6, T1=200e-3, max
         points_sequence = points_90 + points_d1 + points_180
         assert points_sequence < 4097
         #{{{ generating the arbitrary waveform
-        t = r_[0:4096]
+        t = r_[0:int(points_total)]
         freq_sampling = 0.25
         y = exp(1j*2*pi*t[1 : -1]*freq_sampling)
         y[int(points_90) : int(points_90+points_d1)] = 0
@@ -403,8 +404,8 @@ def nutation(t_90_range, spin_echo = False, freq = 14.46e6, T1 = 200e-3):
 #}}}
 date = '181103'
 #id_string = 'nutation_2'
-id_string = 'spin_echo_TL'
-num_cycles = 1 
+id_string = 'spin_echo_2'
+num_cycles = 3 
 t1,t2 = spin_echo(num_cycles = num_cycles)
 #t_90_range = linspace(0.5e-6,3.0e-6,25,endpoint=False)
 #nutation(t_90_range, spin_echo=True)
