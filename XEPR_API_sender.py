dd410850d50d2edeb0280f04c1870560d31f2dc3
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-11-14 18:01:44 -0500

    Functioning scripts that allow for setting value for magnetic field of EPR spectrometer from NMR spectrometer computer using Xepr API

---
+++ XEPR_API_sender.py
@@ -0,0 +1,22 @@
+import socket
+import sys
+
+#IP = "127.0.0.1"
+IP = "jmfrancklab-bruker.syr.edu"
+#IP = "128.230.29.95"
+if len(sys.argv) > 1:
+    IP = sys.argv[1]
+PORT = 6001
+
+MESSAGE = '3403.00'
+print "target IP:", IP
+print "target port:", PORT
+print "message:", MESSAGE
+print "SETTING FIELD TO...", MESSAGE
+
+sock = socket.socket(socket.AF_INET, # Internet
+        socket.SOCK_STREAM) # TCP
+sock.connect((IP, PORT))
+sock.send(MESSAGE)
+sock.close()
+print "FIELD SET TO...", MESSAGE
