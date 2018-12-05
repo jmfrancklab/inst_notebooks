dc7a50524c3ca17c382eea59f780d0bc414f09c3
Pi Account <pi@jmfranck-pi2.syr.edu>
2018-12-05 04:22:07 +0000

    add bridge12 to import * statement

--- Instruments/__init__.py
+++ Instruments/__init__.py
@@ -1,5 +1,6 @@
 from .serial_instrument import SerialInstrument
 from .gds import GDS_scope
 from .afg import AFG
+from .bridge12 import Bridge12
 
-__all__ = ['SerialInstrument','GDS_scope','AFG']
+__all__ = ['SerialInstrument','GDS_scope','AFG',"Bridge12"]
