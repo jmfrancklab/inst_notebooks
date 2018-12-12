86e77ea2bf6c8305da82c6b350e39ed107cb113c
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-12-12 15:58:31 -0500

    add docstring for capture analytic

--- capture_analytic.py
+++ capture_analytic.py
@@ -1,19 +1,13 @@
 #{{{ Program doc
-r'''Use this program to capture a single S11 (reflection) measurement
-    Must use the appropriate set up of the power splitter (PS) and
-    reference channel (CH1). Set up as follows (also see AAB-2, 7/12/2018
-    yellow tab):
-    CH1 (AFG) -*ref*-> CH1 (GDS) *Use coax labeled 'REF'
-    CH2 (AFG) --> PS (PORT 1) --> PS (S) --> DUT --> PS (2) --> CH2 (GDS)
-    Sends out programmed waveform on both channels of the AFG, with
-    the option to correct for amplitude variation (due to use of
-    the power splitter), so that the reflection is calculated
-    correctly (using *proc_chirp.py*)
-    There are two options for a programmed waveform:
-        (1) A proper chirp from 5 to 25 MHz,
-    and (2) A pulse of a specified frequency
-    These options are chosen by the :pulse_90: boolean,
-    where True is (2) and False is (1).
+r'''
+Use this program to capture a scope trace and display the analytic signal
+(frequency is hard-coded in here)
+
+Can capture a series of differently named datasets that are written to the same
+hdf5 file (filename also hard coded, one dataset per node).  Give a list of the
+dataset names.  Any dataset that doesn't exist will be captured from the scope.
+To clear/overwrite the last dataset in the list, give -f as an argument, as
+well.
 '''
 #}}}
 from Instruments import *
@@ -28,7 +22,7 @@ start = time.time()
 fl = figlist_var()
 
 args = sys.argv[1:]
-filename = '181212_JFAB_pulse.h5'
+filename = '181212_JFAB_pulse_trial2.h5'
 if '-f' in args:
     force_acq = True
     args.pop(args.index('-f'))
