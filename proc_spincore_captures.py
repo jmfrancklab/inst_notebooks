0be5ced49762f59058ecbf29fb8d3461f2bf0288
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-12-12 18:57:15 -0500

    Further testing with alternate phase cylce

--- proc_spincore_captures.py
+++ proc_spincore_captures.py
@@ -2,13 +2,14 @@ from Instruments import *
 from pyspecdata import *
 
 fl = figlist_var()
-filename = '181212_test_steps.h5'
-for this_num in r_[1,2,3,4]:
+filename = '181212_test_steps_2.h5'
+for this_num in r_[1,2]:
     dataset = str(this_num)
     if dataset == '1':
         label = '0,2'
     elif dataset == '2':
-        label = '1,2'
+        #label = '1,2'
+        label = '1,3' # for *steps_2 only
     elif dataset == '3':
         label = '0,3'
     elif dataset == '4':
