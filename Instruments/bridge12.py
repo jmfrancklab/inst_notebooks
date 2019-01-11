2405b5679099048dc15b9ca4767ca56fecd2bd57
John Franck (desktop) <jmfranck@syr.edu>
2019-01-11 17:37:36 -0500

    JF and SB worked together to add in some of the safety code also includes SB's earlier additions of frq sweep to class

--- Instruments/bridge12.py
+++ Instruments/bridge12.py
@@ -1,14 +1,8 @@
-
 # coding: utf-8
-
 # ## Initialization
 #  
 # To run on a different computer, run ``jupyter notebook --ip='*'`` copy and paste address and replace ``localhost`` with
 # ``jmfranck-pi2.syr.edu`` (if on the internet) or ``192.168.1.20`` (if on local network)
-
-# In[11]:
-
-
 from serial.tools.list_ports import comports
 from serial import Serial
 import time
@@ -20,6 +14,10 @@ class Bridge12 (Serial):
         assert len(portlist)==1, "I need to see exactly one Arduino Due hooked up to the Raspberry Pi"
         thisport = portlist[0]
         super(self.__class__, self).__init__(thisport, timeout=3, baudrate=115200)
+        # this number represents the highest possible reasonable value for the
+        # Rx power -- it is lowered as we observe the Tx values
+        self.safe_rx_level_int = 80
+        self.frq_sweep_10dBm_has_been_run = False
     def bridge12_wait(self):
         #time.sleep(5)
         def look_for(this_str):
@@ -78,7 +76,6 @@ class Bridge12 (Serial):
             if result == setting:
                 return
         raise RuntimeError("After checking status 10 times, I can't get the waveguide to change")
-        
     def ampstatus_int_singletry(self):
         self.write('ampstatus?\r')
         return int(self.readline())
@@ -108,7 +105,6 @@ class Bridge12 (Serial):
             if result == setting:
                 return
         raise RuntimeError("After checking status 10 times, I can't get the amplifier to turn on/off")
-        
     def rfstatus_int_singletry(self):
         self.write('rfstatus?\r')
         retval = self.readline()
@@ -129,7 +125,6 @@ class Bridge12 (Serial):
         return f
     def set_rf(self,setting):
         """set *and check* microwave power
-
         Parameters
         ==========
         s: Serial object
@@ -145,7 +140,6 @@ class Bridge12 (Serial):
             if result == setting:
                 return
         raise RuntimeError("After checking status 10 times, I can't get the mw power to turn on/off")
-
     def power_int_singletry(self):
         self.write('power?\r')
         return int(self.readline())
@@ -160,6 +154,11 @@ class Bridge12 (Serial):
     def set_power(self,dBm):
         """set *and check* power
 
+        Need to have 2 safeties for set_power:
+
+        1. When power is increased above 10dBm, the power is not allowed to increase by more than 3dBm above the current power.
+        2. When increasing the power, call the power reading function.
+
         Parameters
         ==========
         s: Serial object
@@ -172,17 +171,28 @@ class Bridge12 (Serial):
             raise ValueError("You are not allowed to use this function to set a power of greater than 15 dBm for safety reasons")
         elif setting < 0:
             raise ValueError("Negative dBm -- not supported")
+        elif setting >= 100:
+            if not self.frq_sweep_10dBm_has_been_run:
+                raise RuntimeError("Before you try to set the power above 10 dBm, you must first run a tuning curve at 10 dBm!!!")
+            if not hasattr(self,'cur_pwr_int'):
+                raise RuntimeError("Before you try to set the power above 10 dBm, you must first set a lower power!!!")
+            if setting > 30+self.cur_pwr_int: 
+                raise RuntimeError("Cannot raise power by this much! We are over 10dBm.")
         self.write('power %d\r'%setting)
         for j in range(10):
             result = self.power_int()
             if result == setting:
+                self.cur_pwr_int = result
                 return
         raise RuntimeError("After checking status 10 times, I can't get the power to change")
     def rxpowermv_int_singletry(self):
         self.write('rxpowermv?\r')
         retval = self.readline()
-        #print retval
-        return int(retval)
+        retval = int(retval)
+        if retval > self.safe_rx_level_int:
+            self.safe_shutdown()
+            raise RuntimeError("Read an unsafe Rx level of %0.fmV"%(retval/10))
+        return retval
     def rxpowermv_float(self):
         "need two consecutive responses that match"
         for j in range(3):
@@ -231,10 +241,79 @@ class Bridge12 (Serial):
         "return the frequency, in kHz (since it's set as an integer kHz)"
         self.write('freq?\r')
         return int(self.readline())
+    def freq_sweep(self,freq):
+        """Sweep over an array of frequencies.
+
+        Also includes a safety for the return power. 
+
+        Parameters
+        ==========
+        s: Serial object
+            gives the connection
+        freq: array of floats
+            frequencies in Hz
+        Returns
+        =======
+        rxvalues: array
+            An array of floats, same length as freq, containing the receiver (reflected) power in dBm at each frequency.
+        txvalues: array
+            An array of floats, same length as freq, containing the transmitted power in dBm at each frequency.
+        """    
+        rxvalues = zeros(len(freq))
+        txvalues = zeros(len(freq))
+        if not self.frq_sweep_10dBm_has_been_run:
+            if self.cur_pwr_int != 100:
+                self.safe_shutdown()
+                raise ValueError("You must run the frequency sweep for the first time at 10 dBm")
+        #FREQUENCY AND RXPOWER SWEEP
+        for j,f in enumerate(freq):
+            generate_beep(500, 300)
+            b.set_freq(f)  #is this what I would put here (the 'f')?
+            txvalues[j] = self.txpowermv_float()
+            # here is where I include the rxpower safety: is this a good spot, or should I include it right before the return function? 
+            # I put it here so that if any of the values are too high WHILE reading, it will immediately turn off.
+            if self.rxpowermv_float() < (self.txpowermv_float()/1.2): 
+                rxvalues[j] = self.rxpowermv_float()
+            else:                     
+                self.safe_shutdown()
+                raise RuntimeError("Ratio of Rxpower to Txpower is too high!!!"),
+        if self.cur_pwr_int == 100:
+            self.frq_sweep_10dBm_has_been_run = True
+            # reset the safe rx level to the top of the tuning curve at 10 dBm
+            # (this is the condition where we are reflecting 10 dBm onto the Rx diode)
+            self.safe_rx_level_int = int(10*rxvalues.max())
+        return rxvalues, txvalues
+    # ### Need an increase_power_zoom function for zooming in on the tuning dip:
+    def increase_power_zoom(self, dBm, freq):
+         """Sweep over an array of frequencies.
+        Parameters
+        ==========
+        s: Serial object
+            gives the connection
+        dBm: power value in dBm
+        freq: array of floats
+            frequencies in Hz
+        Returns
+        =======
+        rxvalues: array
+            An array of floats, same length as freq, containing the receiver (reflected) power in dBm at each frequency.
+        txvalues: array
+            An array of floats, same length as freq, containing the transmitted power in dBm at each frequency.
+        """    
+        rx = ['rx'].flatten()
+        fq = ['freq'].flatten
+        rx1 = rx[1:]
+        fq1 = fq[1:]
+        md = (max(rx1)+min(rx1))/2
+        crossovers = diff(rx1<md)
+        halfway_idx = argwhere(diff(rx1<md))
+        x_crossings = fq1[halfway_idx].flatten()
+        freq = linspace(x_crossings[0], x_crossings[1], 1000)
+        self.freq_sweep(freq)
     def __enter__(self):
         self.bridge12_wait()
         return self
-    def __exit__(self, exception_type, exception_value, traceback):
+    def safe_shutdown(self):
         try:
             self.set_power(0)
             self.set_rf(False)
@@ -245,5 +324,6 @@ class Bridge12 (Serial):
             self.write('rfstatus %d\r'%0)
             self.write('wgstatus %d\r'%0)
         self.close()
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.safe_shutdown()
         return
-
