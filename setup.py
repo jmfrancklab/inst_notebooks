7452b47dad91c42d4a5af4d115e409e02aaaa3fa
Alec Beaton (SU Network) <aabeaton@syr.edu>
2019-05-10 18:47:43 -0400

    made installable

---
+++ setup.py
@@ -0,0 +1,26 @@
+#from setuptools import setup
+import setuptools # I think this is needed for the following
+from numpy.distutils.core import Extension,setup
+from distutils.spawn import find_executable
+
+ext_modules = []
+execfile('Instruments/version.py')
+
+setup(
+    name='francklab_instruments',
+    author='Beaton,Betts,Franck',
+    version=__version__,
+    packages=setuptools.find_packages(),
+    license='LICENSE.md',
+    author_email='jmfranck@notgiven.com',
+    url='http://github.com/jmfrancklab/inst_notebooks',
+    description='object-oriented control of various instruments',
+    long_description="Bridge12 amplifier, GwInstek AFG and oscilloscope",
+    install_requires=[
+        "numpy",
+        "h5py",
+        "pyserial>=3.0",
+        ],
+    ext_modules = ext_modules,
+    entry_points={},
+)
