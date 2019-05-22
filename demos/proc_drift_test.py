3a615f777012987801c12e183a9147b03d11faff
Alec Beaton (SU Network) <aabeaton@syr.edu>
2019-05-22 19:15:16 -0400

    Processing high power drift tests

--- demos/proc_drift_test.py
+++ demos/proc_drift_test.py
@@ -1,12 +1,12 @@
 from pyspecdata import *
-data = load('190522_drift_test_air_23dBm.npz')
+data = load('190522_drift_test_air_28dBm_3min_2.npz')
 f_axis = data[data.files[0]]
 rx_axis = data[data.files[1]]
 t_axis = data[data.files[2]]
-
-
+print f_axis
 for x in r_[0:len(f_axis)]:
-    plot(t_axis[x,:80],rx_axis[x,:80]/10.,'o-',label='%0.5f GHz'%(f_axis[x]*1e-9))
+    plot(t_axis[x,:50],rx_axis[x,:50]/10.,'o-',label='%0.7f GHz'%(f_axis[x]*1e-9))
 xlabel('time (sec)')
 ylabel('receiver (mV)')
-legend();show()
+grid();legend();show()
+
