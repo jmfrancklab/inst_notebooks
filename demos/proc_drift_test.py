cc18f0bed09d444fa8c39d2ae2133143f7695782
aabeaton <aabeaton@syr.edu>
2019-05-22 17:39:14 -0400

    Changing drift test to no longer attempt the contour, but rather just plot receiver vs time for each frequency

--- demos/proc_drift_test.py
+++ demos/proc_drift_test.py
@@ -1,9 +1,15 @@
 from pyspecdata import *
-data = load('190521_drift_test_air_15dBm.npz')
+data = load('190522_drift_test_air_23dBm.npz')
 f_axis = data[data.files[0]]
 rx_axis = data[data.files[1]]
 t_axis = data[data.files[2]]
 
+print shape(t_axis)
+print shape(rx_axis)
+
 figure()
-contourf(t_axis,f_axis[:,newaxis]*ones_like(t_axis),rx_axis)
-show()
+for x in r_[0:len(f_axis)]:
+    plot(t_axis[x,:80],rx_axis[x,:80]/10.,'o-',label='%0.5f GHz'%(f_axis[x]*1e-9))
+xlabel('time (sec)')
+ylabel('receiver (mV)')
+legend();show()
