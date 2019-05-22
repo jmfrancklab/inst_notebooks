db14e4dbe8a23af8ef5c417293574735f606ea4c
Alec Beaton (SU Network) <aabeaton@syr.edu>
2019-05-22 18:42:25 -0400

    Running high power drift test

--- demos/drift_test_new.py
+++ demos/drift_test_new.py
@@ -6,43 +6,40 @@ import time
 
 with Bridge12() as b:
     b.lock_on_dip(ini_range=(9.80e9,9.83e9))
-    b.zoom(dBm_increment=2)
-    b.zoom(dBm_increment=2)
-    b.zoom(dBm_increment=2)
-    b.zoom(dBm_increment=2)
-    b.zoom(dBm_increment=2)
-
-
-
+    b.zoom(dBm_increment=3)
+    b.zoom(dBm_increment=3)
+    b.zoom(dBm_increment=3)
+    b.zoom(dBm_increment=3)
+    b.zoom(dBm_increment=3)
 
 
     result = b.tuning_curve_data
-    f_axis = result['23dBm_freq']
+    faxis = result['28dBm_freq']
     dip_index = faxis.argmin()
     
     f_axis = r_[faxis[0],faxis[dip_index-3],faxis[dip_index],faxis[dip_index+3],faxis[-1]]
     b.set_rf(False)
 
     sleep_time = 2.0
-    collect_time = 3*60.0
+    collect_time = 2*60.0
     time_pts = int(collect_time/sleep_time)
     rx_array = zeros((len(f_axis),time_pts),dtype=int32)
     t_array = zeros((len(f_axis),time_pts),dtype=double)
     for j,thisfreq in enumerate(f_axis):
         print "FREQUENCY POINT...",j,"which is",thisfreq
         k = 0
-        start = time.time()
         b.set_rf(True)
         b.set_freq(thisfreq)
-        b.set_power(23)
+        start = time.time()
+        b.set_power(28)
         while time.time() - start < collect_time:
             time.sleep(sleep_time)
-            rx_array[j,k] = b.rxpowermv_float()
+            rx_array[j,k] = b.rxpowermv_int_singletry()
             t_array[j,k] = time.time() - start
             k += 1
         b.set_rf(False)
         time.sleep(10)
-    id_string = '190522_drift_test_air_23dBm_3min'
+    id_string = '190522_drift_test_air_28dBm_3min'
     savez(id_string,freq=f_axis,rx=rx_array,t=t_array)
 def plot_all():
     figure()
