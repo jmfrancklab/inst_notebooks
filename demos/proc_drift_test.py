9639bd43893789061d32b8ef360d7112f33e5cb4
Alec Beaton (SU Network) <aabeaton@syr.edu>
2019-05-23 10:53:35 -0400

    Adding important correction to drift test where dip_index is set to minimm of rx array not frequency array

--- demos/proc_drift_test.py
+++ demos/proc_drift_test.py
@@ -1,11 +1,12 @@
 from pyspecdata import *
-data = load('190522_drift_test_air_27dBm_3min.npz')
+data = load('190523_drift_test_oil_27dBm_3min_75DC_2.npz')
 f_axis = data[data.files[0]]
 rx_axis = data[data.files[1]]
 t_axis = data[data.files[2]]
 print f_axis
 for x in r_[0:len(f_axis)]:
     plot(t_axis[x,:50],rx_axis[x,:50]/10.,'o-',label='%0.7f GHz'%(f_axis[x]*1e-9))
+title('Oil at 27 dBm')
 xlabel('time (sec)')
 ylabel('receiver (mV)')
 grid();legend();show()
