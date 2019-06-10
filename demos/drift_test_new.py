3fd81944db6c58d6c829218f722c981791ef5670
Alec Beaton (SU Network) <aabeaton@syr.edu>
2019-06-10 12:22:08 -0400

    Drift test using air at 34 dBm for 3 minutes

--- demos/drift_test_new.py
+++ demos/drift_test_new.py
@@ -11,16 +11,18 @@ with Bridge12() as b:
     b.zoom(dBm_increment=3)
     b.zoom(dBm_increment=3)
     b.zoom(dBm_increment=3)
-    b.zoom(dBm_increment=3)
     b.zoom(dBm_increment=2)
+    b.zoom(dBm_increment=1)
+    b.zoom(dBm_increment=2)
+    b.zoom(dBm_increment=1)
     result = b.tuning_curve_data
     
-    faxis = result['33dBm_freq']
-    dip_index = result['33dBm_rx'].argmin()
+    faxis = result['34dBm_freq']
+    dip_index = result['34dBm_rx'].argmin()
     
     f_axis = r_[faxis[dip_index-5],faxis[dip_index-2],faxis[dip_index],faxis[dip_index+2],faxis[dip_index+5]]
     sleep_time = 2.0
-    collect_time = 2*60.0
+    collect_time = 3*60.0
     time_pts = int(collect_time/sleep_time)
     rx_array = zeros((len(f_axis),time_pts),dtype=int32)
     t_array = zeros((len(f_axis),time_pts),dtype=double)
@@ -30,7 +32,7 @@ with Bridge12() as b:
         b.set_rf(True)
         b.set_freq(thisfreq)
         start = time.time()
-        b.set_power(33)
+        b.set_power(34)
         while time.time() - start < collect_time:
             time.sleep(sleep_time)
             rx_array[j,k] = b.rxpowermv_int_singletry()
@@ -38,7 +40,7 @@ with Bridge12() as b:
             k += 1
         b.set_rf(False)
         time.sleep(10)
-    id_string = '190607_Katie_drift_test_air_33dBm_iris'
+    id_string = '190610_Katie_drift_test_air_34dBm_iris'
     savez(id_string,freq=f_axis,rx=rx_array,t=t_array)
     
     
