2df459895b01bc662c5989180e68f2c4f1af36b2
Alec Beaton (SU Network) <aabeaton@syr.edu>
2019-06-14 13:29:27 -0400

    Updating with a requested noise measurement from JF

--- proc_noise_2ch.py
+++ proc_noise_2ch.py
@@ -330,6 +330,7 @@ for date,id_string,numchan,gain_factor in [
         ('190614','test_4',2,gain_factor_new), # Triax, probe v2.0
         ('190614','test_5',2,gain_factor_new), # Coax, probe v2.0
         ('190614','test_6',2,gain_factor_new), # Coax, DNP probe 
+        ('190614','test_7',2,gain_factor_new), # Coax, DNP probe 
     ]:
     # }}}
     # {{{ plot labels
