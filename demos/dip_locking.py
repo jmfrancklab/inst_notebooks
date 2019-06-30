61ca7efcd9b4a398b1a63b4cbc8f131f005bfb61
Alec Beaton (SU Network) <aabeaton@syr.edu>
2019-06-30 18:02:17 -0400

    Tests that Katie ran

--- demos/dip_locking.py
+++ demos/dip_locking.py
@@ -20,8 +20,8 @@ if run_bridge12:
         b.zoom(dBm_increment=3)
         b.zoom(dBm_increment=3)
         b.zoom(dBm_increment=3)
-        b.zoom(dBm_increment=3)
-        #b.zoom(dBm_increment=1)
+        b.zoom(dBm_increment=2)
+        b.zoom(dBm_increment=1)
         #b.zoom(dBm_increment=1) #Ends at 36 dBm
         manual_iris_adjust = False
         if manual_iris_adjust:
