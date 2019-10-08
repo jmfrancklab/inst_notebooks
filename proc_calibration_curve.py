f2aea8cc20913d3f7475268fff13c3ddc757dce0
aabeaton <aabeaton@syr.edu>
2019-10-08 16:48:50 -0400

    Code for taking calibration curve, here for HP frequency source

---
+++ proc_calibration_curve.py
@@ -0,0 +1,14 @@
+from pyspecdata import *
+
+id_string = '191008_HP_calib_2'
+
+data = load(getDATADIR(exp_type='test_equip')+id_string+'.npz')
+
+x_axis = data[data.files[0]]
+y_axis = data[data.files[1]]
+
+figure()
+plot(x_axis,y_axis,'o-')
+xlabel('programmed power')
+ylabel('output power')
+show()
