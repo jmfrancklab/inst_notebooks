cdec32ac4ff2501cb51cfa874f132d64a92edb47
Alec Beaton (SU Network) <aabeaton@syr.edu>
2019-10-09 12:53:29 -0400

    Calibration curves using Bridge12 internal frequency source

--- proc_calibration_curve.py
+++ proc_calibration_curve.py
@@ -1,6 +1,6 @@
 from pyspecdata import *
 
-id_string = '191009_b12_calib_1'
+id_string = '191009_b12_calib_2'
 
 data = load(getDATADIR(exp_type='test_equip')+id_string+'.npz')
 
@@ -8,7 +8,8 @@ x_axis = data[data.files[0]]
 y_axis = data[data.files[1]]
 
 figure()
+title('Bridge12 Calibration Curve')
 plot(x_axis-35,y_axis,'o-')
-xlabel('programmed power')
+xlabel('programmed power (minus 35)')
 ylabel('output power')
 show()
