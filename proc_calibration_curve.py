93bf8ef05b4976d0c39958364c599fe361c2abda
aabeaton <aabeaton@syr.edu>
2019-10-09 13:10:27 -0400

    Calibration curve for Bridge12 data

--- proc_calibration_curve.py
+++ proc_calibration_curve.py
@@ -1,6 +1,6 @@
 from pyspecdata import *
 
-id_string = '191009_b12_calib_1'
+id_string = '191009_b12_calib_2'
 
 data = load(getDATADIR(exp_type='test_equip')+id_string+'.npz')
 
@@ -8,8 +8,8 @@ x_axis = data[data.files[0]]
 y_axis = data[data.files[1]]
 
 figure()
-title('HP Source Calibration Curve')
-plot(x_axis,y_axis,'o-')
-xlabel('programmed power (dBm)')
+title('Bridge12 Source Calibration Curve')
+plot(x_axis-35,y_axis,'o-')
+xlabel('programmed power - 35 (dBm)')
 ylabel('output power (dBm)')
 show()
