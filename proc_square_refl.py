7fea2a1d0b58d46be55e6161e4aefe8cda310e3e
Alec Beaton (SU Network) <aabeaton@syr.edu>
2020-01-10 18:53:51 -0500

    Recent modifications to time domain tuning, provides more reasonable Q

--- proc_square_refl.py
+++ proc_square_refl.py
@@ -1,32 +1,25 @@
 from pyspecdata import *
 from scipy.optimize import leastsq
-#import logging
-#init_logging(level=logging.DEBUG)
-#{{{ boolean statement for processing data before modification to generate chirp
-#   that sets voltage of ref (CH1 of scope) and DUT (CH2 of scope) to same value
-corrected_volt = True
-#}}}
+
 fl = figlist_var()
-#with figlist_var() as fl:
-#    fl.next('$S_{11}$ : phase', legend=True)
-#expno=0
+
 for date, id_string,corrected_volt in [
         #('180806','pulse_reflection',True),
         #('181001','sprobe_t2',True),
         #('181001','sprobe_t4',True),
         #('181103','probe',True),
-        #('200110','pulse_1',True),
+        #('200110','pulse_2',True),
         ('200110','alex_probe',True),
         ]:
     d = nddata_hdf5(date+'_'+id_string+'.h5/capture1',
                 directory=getDATADIR(exp_type='test_equip'))
     d.set_units('t','s')
     d.name('Amplitude $/$ $V$')
-    fl.next('Raw signal')
-    #d['ch',0] *= 0.5
+    fl.next('Raw signal %s'%id_string)
+    if date == '200110':
+        d['ch',0] *= 0.5
     fl.plot(d['ch',0],alpha=0.5,label='control')
     fl.plot(d['ch',1],alpha=0.5,label='reflection')
-    # {{{ find the analytic signal
     d.ft('t',shift=True)
     d = d['t':(0,None)] # throw out negative frequencies and low-pass
     max_frq = abs(d['ch',0]).argmax('t').data # to get the max frequency
@@ -37,9 +30,10 @@ for date, id_string,corrected_volt in [
     # }}}
     # see PEP-8 https://www.python.org/dev/peps/pep-0008/#other-recommendations
     decay = abs(d['ch',1]).C
-    fl.next('Analytic signal')
+    fl.next('Analytic signal %s'%id_string)
     fl.plot(abs(d['ch',0]), alpha=0.5, label='control')
     fl.plot(abs(d['ch',1]), alpha=0.5, label='reflection')
+    #fl.show();quit()
     # guess the start of the pulse
     ranges = abs(d['ch',0]).contiguous(lambda x:
             x > 0.5*x.data.max()) # returns list of limits for which
@@ -51,18 +45,6 @@ for date, id_string,corrected_volt in [
     f_shift = nddata(frq_test,'f_test')
     # perform and store 200 frequency modulations of signal
     test_array = d['ch',0].C * exp(-1j*2*pi*f_shift*d.fromaxis('t'))
-    showing_fmod = False
-    #{{{ showing the frequency modulation
-    if showing_fmod:
-        fl.next('showing f mod')
-        fl.plot(d['ch',0],label='none')
-        for k in r_[0,50,100,150,199]:
-            modulation_f = test_array.getaxis('f_test')[k]/1e6
-            fl.plot(test_array['f_test',k],label='%0.2f'%modulation_f)
-        fl.next('image f mod')
-        fl.image(test_array)
-        fl.show();quit()
-        #}}}
     test_array.sum('t').run(abs)
     fl.next('test frequency axis')
     fl.plot(test_array,'.')
@@ -70,23 +52,9 @@ for date, id_string,corrected_volt in [
     # abs(sum(waveform)) will be a maximum
     center_frq = test_array.argmax('f_test').data 
     print "found center frequency at %0.5f MHz"%(center_frq/1e6)
-    # }}}
-    # {{{ switch to frequency domain, and relabel using the center frequency
+
     d.ft('t')
-    d.setaxis('t',lambda x: x-center_frq) # center sinc at f=0 Hz
-    # }}}
-    # {{{   try the phasing trick on the pulse
-    #       because the frequency domain should be causal and therefore a
-    #       sum of Lorentzians
-    #
-    #       if the pulse is causal (starts at 0), then its FT is a
-    #       superposition of Lorentzians
-    #
-    #       this works because the sum of the abs of the real part of
-    #       a Lorentzian with correct zero-order phase is a minimum
-    #       for the correctly phased Lorentzian -- anything that
-    #       mixes in the dispersive part increases the sum of the abs
-    #       of the real
+    d.setaxis('t', lambda x: x - center_frq)
     def apply_ph1(ph1,d_orig):
         retval = d_orig.C
         retval *= eixp(-1j*2*pi*ph1*retval.fromaxis('t')) # ph1 is cycles per SW
@@ -105,7 +73,6 @@ for date, id_string,corrected_volt in [
     test_data.run(real).run(abs).sum('t')
     fl.plot(test_data,'.')
     pulse_start = test_data.argmin('t_shift').data
-    # }}}
     d.ift('t')
     d.setaxis('t',lambda x: x-pulse_start)
     # to use the phase of the reference to set both, we could do:
@@ -129,12 +96,6 @@ for date, id_string,corrected_volt in [
     #impulse = exp(-abs(d.fromaxis('t'))/0.01e-6)
     #impulse['t':(None,0)] = 0
     fl.plot(impulse['t':transf_range], alpha=0.5, color='k', label='impulse')
-    # {{{ not sure if needed -- I'm making sure the frequency axes
-    # line up, since the default is to FT back to the same FT
-    # startpoint
-    #d.ft_clear_startpoints('t', t='current')
-    #impulse.ft_clear_startpoints('t', t='current')
-    # }}}
     d.ft('t')
     transf = d['ch',1]/d['ch',0]
     impulse.ft('t')
@@ -144,8 +105,6 @@ for date, id_string,corrected_volt in [
     fl.plot(response.real, alpha=0.5, label='response, real')
     fl.plot(response.imag, alpha=0.5, label='response, imag')
     fl.plot(abs(response), alpha=0.3, linewidth=3, label='response, abs')
-    # }}}
-    #expno += 1 
     decay = d['ch',1].C
     decay.ift('t')
     fl.next('Plotting the decay slice')
@@ -157,14 +116,17 @@ for date, id_string,corrected_volt in [
     fl.plot(abs(decay),':',alpha=0.2,label='Shifted phased analytic reflection')
     decay = abs(decay)['t':(0,6e-6)]
     fl.plot(decay,':',c='k',label='Decay slice')
-    fl.next('Fitting decay')
+    fl.next('Fitting decay %s'%id_string)
     x = decay.getaxis('t')
     ydata = decay.data
     fl.plot(x,ydata, alpha=0.2, human_units=False)
     fitfunc = lambda p, x: p[0]*exp(-x*2*pi*center_frq*(2*p[1]))
-    fl.plot(x, fitfunc(r_[0.5,1.0],x), ':', label='initial fit, Q=30', human_units=False)
+    fl.plot(x, fitfunc(r_[0.1,1.0],x), ':', label='initial fit, Q=30', human_units=False)
     errfunc = lambda p_arg, x_arg, y_arg: fitfunc(p_arg, x_arg) - y_arg
-    p0 = [0.5,1.0]
+    if date == '181103':
+        p0 = [0.5,100.0]
+    elif date == '200110':
+        p0 = [0.1,40.0]
     p1, success = leastsq(errfunc, p0[:], args=(x, ydata))
     print success
     Q = 1./p1[1]
@@ -173,4 +135,4 @@ for date, id_string,corrected_volt in [
     xlabel(r't / $s$')
     ylabel(r'Amplitude / $V$')
     print "Q:",Q
-    fl.show()
+fl.show();quit()
