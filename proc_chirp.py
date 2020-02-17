aabd4c5a150e633d4d653d1cc3f920501f078c66
Alex <guinneak198@github.com>
2020-02-17 16:22:43 -0500

    cleaning up messed with nutation a bit

--- proc_chirp.py
+++ proc_chirp.py
@@ -20,7 +20,7 @@ with figlist_var(filename='chirp.pdf') as fl:
             #('180714','test_chirp',True),   # B_0 = 3396.25 G, sample   
             #('180714','test_chirp_2',True),   # B_0 = 3394.25 G, sample   
             #('180714','test_chirp_3',True),   # B_0 = 3395.75 G, sample   
-            ('200213','alex_coil2',True),   # B_0 = 3395.75 G, sample   
+            ('200214','nutation_coil2_90',True),   # B_0 = 3395.75 G, sample   
             ]:
 #{{{ finding file
         try:
