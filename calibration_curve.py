aab12153685e874da27e4361d7acb51135b777c1
Alec Beaton (SU Network) <aabeaton@syr.edu>
2020-02-28 12:41:25 -0500

    Running calibration test with Bridge12, after amplifier - includes warning statement

--- calibration_curve.py
+++ calibration_curve.py
@@ -2,18 +2,22 @@ from pyspecdata import *
 from Instruments import Bridge12,HP8672A, prologix_connection, gigatronics
 import time
 
-id_string = '200227_B12_TXOUT'
+id_string = '200227_B12_MWOUT_1'
 
 start_power = 0
-stop_power = 10 
+stop_power = 40 
 # If running through amplifier (Bridge12) do not exceed
 # -5 dBm (with 10 dB coupler) 
 # OR +3 dBm (with 20 dB couplers) or you will damage the meter!!
-power_points = 10
+power_points = 80
 HP_powers = linspace(start_power,stop_power,power_points)
 
 raw_points = 250
 raw_powers = zeros(raw_points*power_points)
+
+raw_input("THIS FILE EXPECTS YOU TO HAVE MWOUT ATTACHED TO THE 20 DB COUPLER,
+        WITH THE COUPLED PORT INTO THE METER AND THE OUT PORT INTO THE NARDA
+        TERMINATOR. DO NOT RUN THIS OTHERWISE. ")
 
 count = 0
 with Bridge12() as b:
@@ -29,7 +33,8 @@ with Bridge12() as b:
                         print("SETTING POWER",hp_setting)
                         print("*** *** ***")
                         b.set_freq(9.85e9)
-                        b.set_power(hp_setting)
+                        #b.set_power(hp_setting)
+                        b.calib_set_power(hp_setting)
                         for x in range(raw_points):
                             raw_powers[count] = g.read_power()
                             count += 1
