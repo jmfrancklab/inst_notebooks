9db61c37e125605a2e503ee2ef680e2e34c27dd0
Alec Beaton (SU Network) <aabeaton@syr.edu>
2020-08-21 20:24:37 -0400

    Collecting PSD

--- collect100.py
+++ collect100.py
@@ -68,8 +68,8 @@ def collect(date,id_string,captures):
     print("shape of data",ndshape(s))
     return start
 
-date = '200820'
-id_string = 'test_1_7'
+date = '200821'
+id_string = 'test_1_2'
 captures = linspace(1,100,100)
 
 print("Starting collection...")
