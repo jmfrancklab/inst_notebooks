32a946eb3340e6c9d1e19aae26514b724f3efe1c
John Franck (laptop) <jmfranck@syr.edu>
2020-10-06 18:51:14 -0400

    check that x axis for gds capture is good

--- Instruments/gds.py
+++ Instruments/gds.py
@@ -203,6 +203,8 @@ class GDS_scope (SerialInstrument):
         # Similarly, use V/div scale to scale the y values of the data
         data_array *= float(param.pop('Vertical Scale'))/0.2 # we saw
         #              empirically that 0.2 corresponds to about 1 division
+        if not all(isfinite(x_axis)):
+            raise ValueError("your x axis is not finite!! len(data_array) is %s and 'Sampling Period' is %s"%(str(len(data_array)),str(param.pop('Sampling Period'))))
         data = nddata(data_array,['t']).setaxis('t',x_axis)
         data.set_units('t',param.pop('Horizontal Units').replace('S','s'))
         data.set_units(param.pop('Vertical Units'))
