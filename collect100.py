199b23e4aca8222eb2f8add408862986edb0965c
Alec Beaton (SU Network) <aabeaton@syr.edu>
2021-01-18 20:06:21 -0500

    Collecting noise PSD at LNA input

--- collect100.py
+++ collect100.py
@@ -68,8 +68,8 @@ def collect(date,id_string,captures):
     print("shape of data",ndshape(s))
     return start
 
-date = '210114'
-id_string = 'noise_closed_6FT_BNC_term2_1'
+date = '210118'
+id_string = 'noise_open_term_atLNA_Re3'
 captures = linspace(1,100,100)
 
 print("Starting collection...")
