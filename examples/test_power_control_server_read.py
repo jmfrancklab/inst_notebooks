ada7d6dc522861a4f2e0d26071841984d85de6b4
John Franck (laptop) <jmfranck@syr.edu>
2021-06-21 15:52:11 -0400

    first atttempt at plotting the log result

--- examples/test_power_control_server_read.py
+++ examples/test_power_control_server_read.py
@@ -1,6 +1,11 @@
 "Reads the output from test_power_control_server.py"
 import os, time, h5py
+import matplotlib as plt
 from numpy import empty
+@FuncFormatter
+def thetime(x, position):
+    result = time.localtime(x)
+    return result.strftime('%I:%M%p')
 with h5py.File('output.h5', 'r') as f:
     log_grp = f['log']
     dset = log_grp['log']
@@ -18,3 +23,13 @@ for j in range(len(read_array)):
             thisrx,
             thispower,
             read_dict[thiscmd])
+ax = plt.gca()
+ax.xaxis.set_major_formatter(thedate)
+for whichfield in ['Rx', 'power']:
+    ax.plot(read_array['time'], read_array[whichfield],
+            label=whichfield)
+mask = read_array['cmd'] != 0
+for thistime in read_array['time'][mask]:
+    plt.axvline(x=thistime)
+ax.legend(**dict(bbox_to_anchor=(1.05,1), loc=2, borderaxespad=0.))
+plt.tight_layout()
