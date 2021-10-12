2c42622259b6b7c674b18f7cddfd70edd7027c11
John Franck (bruker computer) <jmfranck@syr.edu>
2021-10-12 17:07:32 -0400

    add two modes of acquiring the tune curve

--- tune_capture.py
+++ tune_capture.py
@@ -18,8 +18,40 @@ print("setting to linear scale and 33 dB atten")
 h["LogScaleEnab"].value = False
 h["PowerAtten"].value = 33
 n_points = int(h["DataRange"][1])
-y_data = np.zeros(n_points)
-for i in range(0, n_points):
-    y_data[i] += h["Data"][i]
-plt.plot(y_data)
+tune_data = {}
+h["RefArm"].value = "On"
+for mode_zoom in [1,2,4,8]:
+    y_data = np.zeros(n_points)
+    x_data = np.linspace(-0.5/mode_zoom,0.5/mode_zoom,n_points)
+    h['ModeZoom'].value = mode_zoom
+    time.sleep(2.0)
+    print("capture now...")
+    for i in range(0, n_points):
+        y_data[i] += h["Data"][i]
+    tune_data['y%d'%mode_zoom] = y_data
+    tune_data['x%d'%mode_zoom] = x_data
+
+tune_data_hpnoref = {}
+h["RefArm"].value = "Off"
+h["PowerAtten"].value = 20
+time.sleep(2.0) # takes longer to turn up power
+for mode_zoom in [1,2,4,8]:
+    y_data = np.zeros(n_points)
+    x_data = np.linspace(-0.5/mode_zoom,0.5/mode_zoom,n_points)
+    h['ModeZoom'].value = mode_zoom
+    time.sleep(2.0)
+    print("capture now...")
+    for i in range(0, n_points):
+        y_data[i] += h["Data"][i]
+    tune_data_hpnoref['y%d'%mode_zoom] = y_data
+    tune_data_hpnoref['x%d'%mode_zoom] = x_data
+h["RefArm"].value = "On"
+
+for thisdata in (tune_data,tune_data_hpnoref):
+    plt.figure()
+    for mode_zoom in [1,2,4,8]:
+        y_data = thisdata['y%d'%mode_zoom]
+        x_data = thisdata['x%d'%mode_zoom]
+        plt.plot(x_data,y_data,'o', alpha=0.5, label='zoom level %d'%mode_zoom)
+    plt.legend()
 plt.show()
