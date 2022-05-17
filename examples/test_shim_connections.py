9591f23ee5f2fe39911c67ffaeb9b64675c19ba2
John Franck (laptop) <jmfranck@syr.edu>
2022-05-17 13:23:56 -0400

    just tweak example to match what we describe better

--- examples/test_shim_connections.py
+++ examples/test_shim_connections.py
@@ -19,7 +19,7 @@ from serial import Serial
 with prologix_connection() as p:
         with HP6623A(prologix_instance=p, address=3) as HP:
             print("*** *** ***")
-            #HP.set_current(2,0.4)
+            HP.set_current(2,0.4)
             print(HP.output(1,0))
             print(HP.output(2,0))
             print(HP.output(3,0))
@@ -33,6 +33,6 @@ with prologix_connection() as p:
             print(HP.check_output(2))
             print(HP.check_output(3))
             print("Current on channel 1 is",HP.get_current(2))
-            #HP.set_voltage(2,0.2)
-            #print("Current on channel 1 is",HP.get_voltage(2))
+            HP.set_voltage(2,0.2)
+            print("Voltage on channel 1 is",HP.get_voltage(2))
             print("*** *** ***")
