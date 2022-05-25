f645a494179efecc342b617fe397224aed50e2ff
John Franck (desktop) <jmfranck@syr.edu>
2022-05-25 18:16:52 -0400

    removed commented out code

--- examples/test_shim_connections.py
+++ examples/test_shim_connections.py
@@ -9,13 +9,6 @@ from Instruments import HP6623A, prologix_connection, gigatronics
 from Instruments.bridge12 import convert_to_power, convert_to_mv
 from serial import Serial
 
-#with prologix_connection() as p:
-#        with gigatronics(prologix_instance=p, address=7) as g:
-#            print("*** *** ***")
-#            g.read_power()
-#            print("*** *** ***")
-#            quit()
-
 with prologix_connection() as p:
         with HP6623A(prologix_instance=p, address=3) as HP:
             print("*** *** ***")
