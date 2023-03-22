1094a1877fcd429451e1e30c28276a6d873fea52
Alex Guinness (lab) <aguinnes@syr.edu>
2023-03-22 09:12:06 -0400

    AG starts on her own but in case she messes things up

---
+++ examples/test_HP_power_source.py
@@ -0,0 +1,16 @@
+"""
+h power source to replace B12 synthesizer
+==========================================
+
+Test that we can turn the power source on and chagne the power/frequency
+"""
+from Instruments import HP8672A, prologix_connection, gigatronics
+from Instruments.bridge12 import convert_to_power, convert_to_mv
+from serial import Serial
+desired_atten_reading = -10 # this should be a negative number!
+output_power = desired_atten_reading + 20
+with prologix_connection() as p:
+    with HP8672A(prologix_instance=p,address=19) as h:
+        h.set_frequency(9.820701e9)#try setting MHz first
+        h.set_power(output_power-35.0)
+
