024f2a3b7e6b9c38156d7327581438ef85bfcbeb
John M. Franck (SU laptop) <jmfranck@syr.edu>
2023-04-06 14:48:16 -0400

    made an actual gigatronics example

--- examples/test_gigatronics.py
+++ examples/test_gigatronics.py
@@ -1,14 +1,5 @@
-import os, time, h5py
-from numpy import empty
-from matplotlib.ticker import FuncFormatter
-from pyspecdata.file_saving.hdf_save_dict_to_group import hdf_save_dict_to_group
-init_logging(level='debug')
-
-with power_control() as p:
-    retval = p.dip_lock(9.81,9.83)
-    p.start_log()
-    this_log = p.stop_log()
-log_dict = this_log.log_dict
-with h5py.File('output.h5', 'a') as f:
-    log_grp = f.create_group('log') # normally, I would actually put this under the node with the data
-    hdf_save_dict_to_group(log_grp, this_log.__getstate__())
+from Instruments import HP8672A, prologix_connection, gigatronics
+from Instruments.bridge12 import convert_to_power, convert_to_mv
+with prologix_connection() as p:
+    with gigatronics(prologix_instance=p) as g:
+        print("I see a power of",g.read_power())
