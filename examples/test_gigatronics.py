206352c985b2c0e66ac23849cd79437ec8c3f9ed
John M. Franck (SU laptop) <jmfranck@syr.edu>
2023-04-06 15:19:59 -0400

    Revert "port can only connect to prologix or the server not both?"
    This reverts commit cc31f35d7e1c91a3295692646ed3c7e447b7566a.

--- examples/test_gigatronics.py
+++ examples/test_gigatronics.py
@@ -1,10 +1,5 @@
-from Instruments import HP8672A, prologix_connection, gigatronics, power_control
+from Instruments import HP8672A, prologix_connection, gigatronics
 from Instruments.bridge12 import convert_to_power, convert_to_mv
-with prologix_connection() as prx:
-    with gigatronics(prologix_instance=prx,address=7) as g:
+with prologix_connection() as p:
+    with gigatronics(prologix_instance=p) as g:
         print("I see a power of",g.read_power())
-        input("start the server")
-        with power_control() as p:
-            this_f = p.dip_lock(9.81,9.83)
-            p.set_power(10)
-            print("Now the power is", g.read_power())
