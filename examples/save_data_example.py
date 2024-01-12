c480d30e263d40e34263ba1c9dd5bb1ca1d5070e
Alex Guinness <aguinnes@syr.edu>
2024-01-12 12:16:04 -0500

    reviewed changes, nixed spincore calls in example and added comments

--- examples/save_data_example.py
+++ examples/save_data_example.py
@@ -39,16 +39,10 @@ long_delay = 5
 #}}}
 #{{{ function that generates fake data with two indirect dimensions
 def run_scans(indirect_idx, indirect_len, indirect_fields = None, ret_data=None):
-    # there are many changes to this function that seem to be aimed
-    # at making it more dependent on code that is elsewhere
-    # this should be a simple example, so I'm rolling those back
-    ph1_cyc = r_[0,1,2,3]
-    ph2_cyc = r_[0]
     nPhaseSteps = len(ph1_cyc)*len(ph2_cyc)
     data_length = 2*nPoints*nEchoes*nPhaseSteps
     for nScans_idx in range(nScans):
         raw_data = np.random.random(data_length) + np.random.random(data_length) * 1j
-<<<<<<< HEAD
         data_array = []
         data_array[::] = complex128(raw_data[0::2]+1j*raw_data[1::2])
         dataPoints = float(shape(data_array)[0])
